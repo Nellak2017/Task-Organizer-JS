47612bf927bfffea3fca4e0487c5fffaa88eadd3
Nellak2017 <Nellakprime@gmail.com>
2021-06-13 14:21:30 +0300

    Updated the Styles.js and App.jsx.
    Made the background of the Navbar white.
    Made the icon blue.
    Added a drop-down menu in App.jsx for the features.

--- client/task-organizer-js/src/App.jsx
+++ client/task-organizer-js/src/App.jsx
@@ -1,20 +1,48 @@
-import { CssBaseline, Typography, AppBar, Toolbar, Button, IconButton, MenuIcon } from '@material-ui/core';
+import { CssBaseline, Typography, AppBar, Toolbar, Button, IconButton, MenuIcon, Menu, MenuItem} from '@material-ui/core';
 import GridOnIcon from '@material-ui/icons/GridOn';
-import useStyles from './Syles';
+import useStyles from './Styles';
+import { useState } from 'react';
 
 const App = () => {
 
   const classes = useStyles();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  // TODO: Extract the Drop-down menu logic into another component
   return ( 
     <>
       <CssBaseline/>
         <div className="classes.root">
           <AppBar position="static">
-            <Toolbar>
-              <GridOnIcon />
+            <Toolbar className={classes.toolbar}>
+              <GridOnIcon color={'primary'}/>
               <Typography variant="h6" className={classes.title}>
                 Task Organizer   
               </Typography>
+              <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                Features
+              </Button>
+              <Menu
+                id="Features"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose}>Intuitive Interface</MenuItem>
+                  <MenuItem onClick={handleClose}>Multiple Views</MenuItem>
+                  <MenuItem onClick={handleClose}>Schedule Tasks</MenuItem>
+                  <MenuItem onClick={handleClose}>Share Tasks with others</MenuItem>
+              </Menu>
+
             </Toolbar>
           </AppBar>
         </div>
---
+++ client/task-organizer-js/src/Styles.js
@@ -0,0 +1,24 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: 'orange',
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    color: "black",
+  },
+  toolbar: {
+    backgroundColor: "white",
+  },
+  menuDropDown: {
+    color: "black",
+    fontSize: "14px", 
+  }
+}));
+
+export default useStyles;
