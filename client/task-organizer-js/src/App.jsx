b4da107d9c5aadf20f725ddfa4acfb1c924a284d
Connor Keenum <Nellakprime@gmail.com>
2021-06-14 19:26:14 +0300

    Removed lots of old logic, added in Navbar and Global Style sheet for every styled component. Also Added in Routing using React-Router-DOM.

--- client/task-organizer-js/src/App.jsx
+++ client/task-organizer-js/src/App.jsx
@@ -1,35 +1,14 @@
-import { CssBaseline, Typography, AppBar, Toolbar} from '@material-ui/core';
-import GridOnIcon from '@material-ui/icons/GridOn';
-import { makeStyles } from '@material-ui/core';
-import { useState } from 'react';
-import useStyles from './Styles';
-import NavBar from './components/Nav/NavBar.js';
+import {Navbar} from './components';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import GlobalStyle from './globalStyles';
 
 const App = () => {
 
-  const classes = useStyles();
-
-  // TODO: Make the logo and typography beside it a link to the nellakproject.com/
-  // TODO: Make the typography stack beside the logo
-  // TODO: Make everything fit in the second third of the screen
-  // TODO: Get the values for the drop-downs from somewhere else instead of hardcoding it 
-  // TODO: Either use regular CSS for styles or use Styled Components to style your page, JSS isn't functional
-
   return ( 
-    <>
-      <CssBaseline/>
-        <div className={classes.root}>
-          <AppBar position="static">
-            <Toolbar className={classes.toolbar}>
-              <GridOnIcon color={'primary'}/>
-              <Typography variant="h6" className={classes.title}>
-                Task Organizer   
-              </Typography>
-              <NavBar></NavBar>
-            </Toolbar>
-          </AppBar>
-        </div>
-    </>
+    <Router>
+          <GlobalStyle/>
+          <Navbar/>
+    </Router>
    );
 }
  
