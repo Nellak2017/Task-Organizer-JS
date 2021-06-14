518607110eca1c96ce2cea85568e93fa2157dab1
Connor Keenum <Nellakprime@gmail.com>
2021-06-14 23:08:25 +0300

    Assembled the Styled Components together to make the InfoSection Component.

--- client/task-organizer-js/src/components/InfoSection/InfoSection.js
+++ client/task-organizer-js/src/components/InfoSection/InfoSection.js
@@ -1,10 +1,56 @@
-import {InfoSec} from './InfoSection.elements';
+import {
+    InfoSec, 
+    InfoRow, 
+    InfoColumn, 
+    TextWrapper, 
+    TopLine,
+    Heading,
+    Subtitle,
+    ImgWrapper,
+    Img
+} from './InfoSection.elements';
+import {Link} from 'react-router-dom';
+import {Container, Button} from '../../globalStyles';
 
-const InfoSection = () => {
+const InfoSection = ({
+    primary, 
+    lightBg, 
+    imgStart, 
+    lightTopLine, 
+    lightTextDesc, 
+    buttonLabel, 
+    description, 
+    headline, 
+    lightText, 
+    topLine,
+    img, 
+    alt, 
+    start
+}) => {
     return ( 
         <>
-            <InfoSec>
-
+            <InfoSec lightBg={lightBg}>
+                <Container>
+                    <InfoRow imgStart={imgStart}>
+                        <InfoColumn>
+                            <TextWrapper>
+                                <TopLine lightTopLine={lightTopLine}>{topLine}</TopLine>
+                                <Heading lightText={lightText}>{headline}</Heading>
+                                <Subtitle lightTextDesc={lightTextDesc}>{description}</Subtitle>
+                                <Link to='/sign-up'>
+                                    <Button big fontBig primary={primary}>
+                                        {buttonLabel}
+                                    </Button>
+                                </Link>
+                            </TextWrapper>
+                        </InfoColumn>
+                        <InfoColumn>
+                            <ImgWrapper start={start}>
+                                <Img src={img} alt={alt}/>
+                            </ImgWrapper>
+                        </InfoColumn>
+                    </InfoRow>
+                </Container>
             </InfoSec>
         </>
      );
