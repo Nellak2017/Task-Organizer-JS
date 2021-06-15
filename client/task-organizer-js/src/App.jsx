deecb1884cfa6036742299d5d793cd01c1afe16c
Connor Keenum <Nellakprime@gmail.com>
2021-06-15 20:40:56 +0300

    Added ScrollToTop and the Footer Components to App

--- client/task-organizer-js/src/App.jsx
+++ client/task-organizer-js/src/App.jsx
@@ -1,17 +1,20 @@
-import {Navbar} from './components';
+import {Navbar, Footer} from './components';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import GlobalStyle from './globalStyles';
 import Home from './pages/HomePage/Home';
+import ScrollToTop from './components/ScrollToTop';
 
 const App = () => {
 
   return ( 
     <Router>
           <GlobalStyle/>
+          <ScrollToTop />
           <Navbar/>
           <Switch>
             <Route path="/" exact component={Home}></Route>
           </Switch>
+          <Footer/>
     </Router>
    );
 }
