a233390faa0b8bfe56e66e949fac82ed0a3077e3
Connor Keenum <Nellakprime@gmail.com>
2021-06-17 23:51:35 +0300

    Temporarily Replaced NavBar with Side Nav So it is easier to see when making the styled component.

--- client/task-organizer-js/src/App.jsx
+++ client/task-organizer-js/src/App.jsx
@@ -1,8 +1,12 @@
-import {Navbar, Footer} from './components';
+import {Navbar, Footer, SideNav} from './components';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import GlobalStyle from './globalStyles';
 import Home from './pages/HomePage/Home';
+import OrganizerMain from './pages/OrganizerMain/OrganizerMain';
 import ScrollToTop from './components/ScrollToTop';
+
+// TODO: Remove SideNav from the main page, and put in Navbar
+// TODO: Finish up SideNav, SideNav.elements, OrganizerMain.js, and Data.js from \OrganizerMain
 
 const App = () => {
 
@@ -10,9 +14,10 @@ const App = () => {
     <Router>
           <GlobalStyle/>
           <ScrollToTop />
-          <Navbar/>
+          <SideNav/>
           <Switch>
             <Route path="/" exact component={Home}></Route>
+
           </Switch>
           <Footer/>
     </Router>
