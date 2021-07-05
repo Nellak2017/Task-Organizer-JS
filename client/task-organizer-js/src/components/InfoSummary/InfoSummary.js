8b3cdd7d7ca35b9c7c36d82e7a5407697806bb3f
Connor Keenum <Nellakprime@gmail.com>
2021-07-05 16:59:16 +0300

    Styled the Collapsable and the Collapsable Content area so that on click of icon, it smoothly transistions from visible to invisible while auto adjusting elements as well. It works pretty well, though still needs testing.

--- client/task-organizer-js/src/components/InfoSummary/InfoSummary.js
+++ client/task-organizer-js/src/components/InfoSummary/InfoSummary.js
@@ -1,34 +1,50 @@
+import { useState } from 'react';
 import {
     SummaryContainer,
     SummaryDropDown,
     SummaryDropDownContainer,
     SummaryDropDownItem,
-    SummaryInfoContainer
+    SummaryInfoCollapsable
 } from './InfoSummary.elements.js';
 import { IconContext } from 'react-icons/lib';
 import * as GoIcons from 'react-icons/go';
 
-// TODO: Get the Data from Somewhere else, this is just magic values rn
-// TODO: Handle an onclick event for the SummaryDropDownItem 
-// TODO: Get Rid of the Border Color Scaffolding
+// TODO: With the Data, map the Data to the Components like in the other components
+// TODO: Rename the Styled Compnents to be more semantic, this is a CSS Collapsable,w/content
 
 const InfoSummary = () => {
+
+    const [click, setClick] = useState(true);
+
+    const handleClick = () => setClick(!click);
+
     return (
         <>
             <IconContext.Provider value={{ color: '#fff', size: '1.5rem' }}>
                 <SummaryContainer>
                     <SummaryDropDown>
                         <SummaryDropDownContainer>
-                            <GoIcons.GoTriangleDown />
+                            <GoIcons.GoTriangleDown className="icon" onClick={handleClick} />
                             <p>High Priority Tasks</p>
                         </SummaryDropDownContainer>
                         <SummaryDropDownContainer>
                             <SummaryDropDownItem>
-                                <p>See</p> 
+                                <p>See</p>
                                 <p>all</p>
                             </SummaryDropDownItem>
                         </SummaryDropDownContainer>
                     </SummaryDropDown>
+                </SummaryContainer>
+                <SummaryContainer style={{ marginTop: '0' }}>
+                    {
+                        click ?
+                            <SummaryInfoCollapsable expanded>
+                                <p>Collapsable Content</p>
+                            </SummaryInfoCollapsable> :
+                            <SummaryInfoCollapsable >
+                                <p>Collapsable Content</p>
+                            </SummaryInfoCollapsable>
+                    }
                 </SummaryContainer>
             </IconContext.Provider>
         </>
