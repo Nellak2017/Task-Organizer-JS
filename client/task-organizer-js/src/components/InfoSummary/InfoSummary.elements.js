375cae4e20062e632d206b8f6903b587142d59ce
Connor Keenum <Nellakprime@gmail.com>
2021-07-05 16:58:58 +0300

    Styled the Collapsable and the Collapsable Content area so that on click of icon, it smoothly transistions from visible to invisible while auto adjusting elements as well. It works pretty well, though still needs testing.

--- client/task-organizer-js/src/components/InfoSummary/InfoSummary.elements.js
+++ client/task-organizer-js/src/components/InfoSummary/InfoSummary.elements.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { keyframes } from 'styled-components';
 
 // Holds the InfoSummary Component and gives it proper placement
 export const SummaryContainer = styled.div`
@@ -7,13 +8,19 @@ export const SummaryContainer = styled.div`
     align-items: center;
     justify-content: center;
     color: #f5f5f5;
-    margin: 3rem 0;
+    margin: 3rem 0 1rem 0;
     font-size: 2rem;
 
-    border: 1px solid red;
+    //border: 1px solid red;
 
     & .icon {
-        // Change Icon styles dynamically using this as it's classname
+        margin-right: 1rem;
+    }
+
+    & .icon:hover {
+        transition: 350ms;
+        color: #1a83ff !important;
+        cursor: pointer;
     }
 `;
 
@@ -27,13 +34,7 @@ export const SummaryDropDown = styled.section`
     justify-content: space-between;
     width: 47rem;
 
-    border: 1px solid green;
-
-    // Give all elements inside me some styles
-    & * {
-        margin-right: 1rem;
-        border: 1px solid blue;
-    }
+    border-bottom: 1px solid rgba(68,76,86,.5);
 `;
 // This is to hold the elements inside of the Summary Drop down
 // So that items can be correctly positioned
@@ -41,8 +42,8 @@ export const SummaryDropDownContainer = styled.span`
     display: flex;
     flex-direction: row;
     align-items: center;
-    border: 1px solid violet;
-    margin: 0;
+    padding-bottom: .25rem;
+    //border: 1px solid green;
 `;
 
 export const SummaryDropDownItem = styled.span`
@@ -51,16 +52,38 @@ export const SummaryDropDownItem = styled.span`
     align-items: center;
     justify-content: center;
     font-size: 1rem;
-    margin: 0;
-    padding: 0;
 
     &:hover {
-        color: #f5f5f5;
+        transition: 350ms;
+        color: #1a83ff !important;
         cursor: pointer;
     }
 `;
 
-// The place where the Threads or Pertinent todos or whatever go
-export const SummaryInfoContainer = styled.div`
+// This will let us smoothly transition from nothing to something
 
+const ExpandingAnimation = keyframes`
+    0% { opacity: 0; min-height: 0; padding: 0; margin: 0;}
+    100% { opacity: 1; min-height: 10rem;}
+`;
+
+const CollapsingAnimation = keyframes`
+    0% { opacity: 1; min-height: 10rem;}
+    100% { opacity: 0; min-height: 0; padding: 0; margin: 0;}
+`;
+
+// The place where the Threads or Pertinent todos or whatever go
+export const SummaryInfoCollapsable = styled.section`
+    animation-name: ${props => props.expanded ?
+        ExpandingAnimation :
+        CollapsingAnimation
+    };
+    animation-duration: 500ms;
+    animation-fill-mode: both;
+    font-size: 1rem;
+    width: 47rem;
+    padding: 1rem;
+    min-height: 10rem;
+    background-color: rgba(44,49,56,1);
+    border: 1px solid rgba(68,76,86,.5);
 `;
