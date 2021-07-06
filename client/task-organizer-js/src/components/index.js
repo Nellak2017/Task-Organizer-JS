e1ce81e614f5f48144c31191d62d9eb570614eba
Connor Keenum <Nellakprime@gmail.com>
2021-07-06 21:48:57 +0300

    Updated Exports.

--- client/task-organizer-js/src/components/index.js
+++ client/task-organizer-js/src/components/index.js
@@ -3,3 +3,4 @@ export { default as InfoSection } from './InfoSection/InfoSection';
 export { default as Footer } from './Footer/Footer';
 export { default as SideNav } from './SideNav/SideNav';
 export { default as InfoSummary } from './InfoSummary/InfoSummary';
+export { default as TableContent } from './TableContent/TableContent'
