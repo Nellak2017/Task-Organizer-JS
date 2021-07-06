0f554895502ca4c7d5bbe7f030c85381c044dd83
Connor Keenum <Nellakprime@gmail.com>
2021-07-06 21:47:19 +0300

    Took away redundant Icon:hover style.Put it in the global stylesheet.

--- client/task-organizer-js/src/components/InfoSummary/InfoSummary.js
+++ client/task-organizer-js/src/components/InfoSummary/InfoSummary.js
@@ -11,8 +11,11 @@ import * as GoIcons from 'react-icons/go';
 
 // TODO: With the Data, map the Data to the Components like in the other components
 // TODO: Rename the Styled Compnents to be more semantic, this is a CSS Collapsable,w/content
+// TODO: Pass the Component you want to use as a Prop to this Component
+//       see: https://www.tutorialsplane.com/react-pass-component-as-prop-with-props-example/
+// TODO: put this https://www.tutorialsplane.com/react-pass-component-as-prop-with-props-example/ in MEM.DEV
 
-const InfoSummary = () => {
+const InfoSummary = ({MyComponent}) => {
 
     const [click, setClick] = useState(true);
 
@@ -39,10 +42,10 @@ const InfoSummary = () => {
                     {
                         click ?
                             <SummaryInfoCollapsable expanded>
-                                <p>Collapsable Content</p>
+                                <MyComponent/>
                             </SummaryInfoCollapsable> :
                             <SummaryInfoCollapsable >
-                                <p>Collapsable Content</p>
+                                <MyComponent/>
                             </SummaryInfoCollapsable>
                     }
                 </SummaryContainer>
