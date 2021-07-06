49fb6ca3cadf517c59fd827a155cab65525fc95e
Connor Keenum <Nellakprime@gmail.com>
2021-07-06 21:47:01 +0300

    Took away redundant Icon:hover style.Put it in the global stylesheet.

--- client/task-organizer-js/src/components/InfoSummary/InfoSummary.elements.js
+++ client/task-organizer-js/src/components/InfoSummary/InfoSummary.elements.js
@@ -16,12 +16,6 @@ export const SummaryContainer = styled.div`
     & .icon {
         margin-right: 1rem;
     }
-
-    & .icon:hover {
-        transition: 350ms;
-        color: #1a83ff !important;
-        cursor: pointer;
-    }
 `;
 
 // The Place where there is a brief title, dropdown, 
@@ -32,7 +26,8 @@ export const SummaryDropDown = styled.section`
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
-    width: 47rem;
+    margin-left: 20rem; //  was 0
+    width: 67rem; // was 47rem
 
     border-bottom: 1px solid rgba(68,76,86,.5);
 `;
@@ -63,13 +58,13 @@ export const SummaryDropDownItem = styled.span`
 // This will let us smoothly transition from nothing to something
 
 const ExpandingAnimation = keyframes`
-    0% { opacity: 0; min-height: 0; padding: 0; margin: 0;}
+    0% { opacity: 0; min-height: 0; padding: 0; margin: 0 0 0 20rem;}
     100% { opacity: 1; min-height: 10rem;}
 `;
 
 const CollapsingAnimation = keyframes`
     0% { opacity: 1; min-height: 10rem;}
-    100% { opacity: 0; min-height: 0; padding: 0; margin: 0;}
+    100% { opacity: 0; min-height: 0; padding: 0; margin: 0 0 0 20rem;}
 `;
 
 // The place where the Threads or Pertinent todos or whatever go
@@ -81,7 +76,8 @@ export const SummaryInfoCollapsable = styled.section`
     animation-duration: 500ms;
     animation-fill-mode: both;
     font-size: 1rem;
-    width: 47rem;
+    margin-left: 20rem; //  was 0
+    width: 67rem; // was 47rem
     padding: 1rem;
     min-height: 10rem;
     background-color: rgba(44,49,56,1);
