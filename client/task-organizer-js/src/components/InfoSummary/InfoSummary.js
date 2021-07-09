19fe69d6ceb5ebbe63509b83b6874404f31e1c84
Connor Keenum <Nellakprime@gmail.com>
2021-07-09 16:58:48 +0300

    Made some minor changes

--- client/task-organizer-js/src/components/InfoSummary/InfoSummary.js
+++ client/task-organizer-js/src/components/InfoSummary/InfoSummary.js
@@ -11,7 +11,7 @@ import * as GoIcons from 'react-icons/go';
 import { TableContentData } from "../../pages/OrganizerMain/Data";
 
 // TODO: With the Data, map the Data to the Components like in the other components
-// TODO: Rename the Styled Compnents to be more semantic, this is a CSS Collapsable,w/content
+// TODO: Rename the Styled Components to be more semantic, this is a CSS Collapsable,w/content
 // TODO: Pass the Component you want to use as a Prop to this Component
 //       see: https://www.tutorialsplane.com/react-pass-component-as-prop-with-props-example/
 // TODO: put this https://www.tutorialsplane.com/react-pass-component-as-prop-with-props-example/ in MEM.DEV
