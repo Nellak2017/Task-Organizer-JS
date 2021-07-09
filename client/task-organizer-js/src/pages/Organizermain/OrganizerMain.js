2579167ef44f0bec23be0e5914213ca3acd6775c
Connor Keenum <Nellakprime@gmail.com>
2021-07-09 11:51:25 +0300

    Added comment on a TODO that needs to be done. I need to learn how to pass a Component with Props as a Prop to another Component.

--- client/task-organizer-js/src/pages/Organizermain/OrganizerMain.js
+++ client/task-organizer-js/src/pages/Organizermain/OrganizerMain.js
@@ -1,11 +1,12 @@
+import React from "react";
 import { SideNav, InfoSummary, TableContent } from "../../components";
 
 // TODO: Add Media Queries for Responsive Design
+// TODO: Find out how to pass props to components passed as props
 const OrganizerMain = () => {
     return ( 
         <>
         <SideNav />
-        <InfoSummary MyComponent={TableContent} />
         <InfoSummary MyComponent={TableContent} />
         </>
      );
