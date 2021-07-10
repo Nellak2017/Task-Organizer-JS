ab242e850e44a90c9b8071799d2b4bbf1d135c13
Connor Keenum <Nellakprime@gmail.com>
2021-07-10 19:48:51 +0300

    Mapped to the Data almost perfectly. I still need to find a way to add a component on hover for the SummaryDropDownItem Icon.

--- client/task-organizer-js/src/components/InfoSummary/InfoSummary.js
+++ client/task-organizer-js/src/components/InfoSummary/InfoSummary.js
@@ -8,15 +8,14 @@ import {
 } from './InfoSummary.elements.js';
 import { IconContext } from 'react-icons/lib';
 import * as GoIcons from 'react-icons/go';
-import { TableContentData } from "../../pages/OrganizerMain/Data";
 
-// TODO: With the Data, map the Data to the Components like in the other components
+// TODO: Make iconComponent from Data Hoverable 
 // TODO: Rename the Styled Components to be more semantic, this is a CSS Collapsable,w/content
 // TODO: Pass the Component you want to use as a Prop to this Component
 //       see: https://www.tutorialsplane.com/react-pass-component-as-prop-with-props-example/
 // TODO: put this https://www.tutorialsplane.com/react-pass-component-as-prop-with-props-example/ in MEM.DEV
 
-const InfoSummary = ({MyComponent}) => {
+const InfoSummary = ({ MyComponent, Data, ComponentData }) => {
 
     const [click, setClick] = useState(true);
 
@@ -25,28 +24,41 @@ const InfoSummary = ({MyComponent}) => {
     return (
         <>
             <IconContext.Provider value={{ color: '#fff', size: '1.5rem' }}>
-                <SummaryContainer>
-                    <SummaryDropDown>
-                        <SummaryDropDownContainer>
-                            <GoIcons.GoTriangleDown className="icon" onClick={handleClick} />
-                            <p>High Priority Tasks</p>
-                        </SummaryDropDownContainer>
-                        <SummaryDropDownContainer>
-                            <SummaryDropDownItem>
-                                <p>See</p>
-                                <p>all</p>
-                            </SummaryDropDownItem>
-                        </SummaryDropDownContainer>
-                    </SummaryDropDown>
-                </SummaryContainer>
+                {Data.map((item, key) => {
+                    return (
+                        <SummaryContainer key={key}>
+                            <SummaryDropDown>
+                                <SummaryDropDownContainer>
+                                    <GoIcons.GoTriangleDown className="icon" onClick={handleClick} />
+                                    <p>{item.title}</p>
+                                </SummaryDropDownContainer>
+                                <SummaryDropDownContainer>
+                                    <SummaryDropDownItem>
+                                        {item.text !== '' ?
+                                        item.text.split(/[ ,]+/).map( (item, key) =>
+                                        {
+                                            return(
+                                                <p key={key}>{item}</p>
+                                            );
+                                        }
+                                        ):
+                                        <span>{item.icon}</span>}
+                                    </SummaryDropDownItem>
+                                </SummaryDropDownContainer>
+                            </SummaryDropDown>
+                        </SummaryContainer>
+                    );
+                })
+                }
+
                 <SummaryContainer style={{ marginTop: '0' }}>
                     {
                         click ?
                             <SummaryInfoCollapsable expanded>
-                                <MyComponent data={TableContentData}/>
+                                <MyComponent data={ComponentData} />
                             </SummaryInfoCollapsable> :
                             <SummaryInfoCollapsable >
-                                <MyComponent data={TableContentData}/>
+                                <MyComponent data={ComponentData} />
                             </SummaryInfoCollapsable>
                     }
                 </SummaryContainer>
