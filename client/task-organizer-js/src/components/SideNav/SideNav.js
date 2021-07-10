85d4c5510125b57d98217f7027503d2412c82b6c
Connor Keenum <Nellakprime@gmail.com>
2021-07-10 13:35:22 +0300

    Set the SideNav to be open by default, I feel it is more intutive that way.

--- client/task-organizer-js/src/components/SideNav/SideNav.js
+++ client/task-organizer-js/src/components/SideNav/SideNav.js
@@ -21,7 +21,7 @@ import {
 
 const SideNav = () => {
 
-    const [sidebar, setSidebar] = useState(false);
+    const [sidebar, setSidebar] = useState(true);
 
     const showSidebar = () => setSidebar(!sidebar);
 
