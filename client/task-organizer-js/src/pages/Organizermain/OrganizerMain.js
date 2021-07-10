e8784c0d1640eaf585c753bb4b9370592aec341b
Connor Keenum <Nellakprime@gmail.com>
2021-07-10 20:10:37 +0300

    Mapped through the InfoSummaryData elements to produce each InfoSummaryData viewed on the Screen.

--- client/task-organizer-js/src/pages/Organizermain/OrganizerMain.js
+++ client/task-organizer-js/src/pages/Organizermain/OrganizerMain.js
@@ -1,13 +1,17 @@
-import React from "react";
 import { SideNav, InfoSummary, TableContent } from "../../components";
-
+import { InfoSummaryData, TableContentData } from "../../pages/OrganizerMain/Data";
 // TODO: Add Media Queries for Responsive Design
 // TODO: Find out how to pass props to components passed as props
 const OrganizerMain = () => {
     return ( 
         <>
         <SideNav />
-        <InfoSummary MyComponent={TableContent} />
+        {InfoSummaryData.map( (value,key) => {
+            return (
+                <InfoSummary key={key} MyComponent={TableContent} Data={[value]} ComponentData={TableContentData}/>
+            );
+        })
+        }
         </>
      );
 }
