20fa55da7f421849bb18251fc044dc7bb8ab8bf7
Connor Keenum <Nellakprime@gmail.com>
2021-07-12 18:28:41 +0300

    Added very basic dnd functionality to the table. It is rudimentary but it does its' job for now. In later updates add more natural dnd features.

--- client/task-organizer-js/src/components/TableContent/TableContent.js
+++ client/task-organizer-js/src/components/TableContent/TableContent.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import {
     TaskTable,
     TaskTableRow,
@@ -9,7 +10,7 @@ import { IconContext } from 'react-icons/lib';
 import * as BiIcons from 'react-icons/bi';
 import FormatDue from '../../lib/moment/FormatDue.js';
 
-// TODO: Make Drag and Drop feature where you can drag and drop rows in place
+// TODO: Improve Drag-And-Drop Functionality with react-beautiful-dnd (Search how to do it in tables), see also: https://dev.to/milandhar/drag-and-drop-table-with-react-beautiful-dnd-54ad
 // TODO: Gray out stuff when completed
 // TODO: Add logic for see all
 // TODO: Research e.preventDefault()
@@ -20,46 +21,66 @@ const TableContent = ({ data }) => {
     const [tasks, setTasks] = useState([...data]);
 
     const completeTask = (e, index) => {
-        e.preventDefault(); 
+        e.preventDefault();
         const newTasks = [...tasks];
         const status = newTasks[index].status; // get original status
         newTasks[index].status = status !== "Completed" ? "Completed" : "Processing"; // change to opposite of original status
         setTasks(newTasks);
     };
 
+    const handleOnDragEnd = (result) => {
+        if (!result.destination) return;
+        const items = Array.from(tasks);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
+        setTasks(items);
+    }
+
     return (
         <>
             <IconContext.Provider value={{ color: '#fff', size: '2.5rem' }}>
-                <TaskTable>
-                    <thead>
-                        <TaskTableRow>
-                            <td></td>
-                            <TaskTableHeader>Task</TaskTableHeader>
-                            <TaskTableHeader>Due</TaskTableHeader>
-                            <TaskTableHeader>Priority</TaskTableHeader>
-                            <TaskTableHeader>Status</TaskTableHeader>
-                            <TaskTableHeader>Periodicity</TaskTableHeader>
-                        </TaskTableRow>
-                    </thead>
+                <DragDropContext onDragEnd={handleOnDragEnd}>
+                    <TaskTable>
+                        <thead>
+                            <TaskTableRow>
+                                <td></td>
+                                <TaskTableHeader>Task</TaskTableHeader>
+                                <TaskTableHeader>Due</TaskTableHeader>
+                                <TaskTableHeader>Priority</TaskTableHeader>
+                                <TaskTableHeader>Status</TaskTableHeader>
+                                <TaskTableHeader>Periodicity</TaskTableHeader>
+                            </TaskTableRow>
+                        </thead>
 
-                    <tbody>
-                        {tasks.map((value, key) => {
-                            return (
-                                <TaskTableRow key={key}>
-                                    <TaskTableData className="iconTd">{tasks[key].status === "Completed" ? <BiIcons.BiCheckboxChecked key={key} className="icon" onClick={(e) => completeTask(e, key)} />
-                                        : <BiIcons.BiCheckbox key={key} className="icon" onClick={(e) => completeTask(e, key)} />}</TaskTableData>
+                        <Droppable droppableId="Task Summaries">
+                            {(provided) => (
+                                <tbody {...provided.droppableProps} ref={provided.innerRef}>
+                                    {tasks.map((value, key) => {
+                                        return (
+                                            <Draggable key={value.id} draggableId={value.id} index={key}>
+                                                {(provided) => (
+                                                    <TaskTableRow {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                                                        <TaskTableData className="iconTd">{tasks[key].status === "Completed" ? <BiIcons.BiCheckboxChecked key={key} className="icon" onClick={(e) => completeTask(e, key)} />
+                                                            : <BiIcons.BiCheckbox key={key} className="icon" onClick={(e) => completeTask(e, key)} />}</TaskTableData>
 
-                                    <TaskTableData data-content={value.task}>{value.task}</TaskTableData>
-                                    <TaskTableData className="due" data-content={FormatDue(value.due)}><span>{FormatDue(value.due)}</span></TaskTableData>
-                                    <TaskTableData data-content={value.priority}><span>{value.priority}</span></TaskTableData>
-                                    <TaskTableData data-content={value.status}><span>{value.status}</span></TaskTableData>
-                                    <TaskTableData data-content={value.periodicity}>{value.periodicity}</TaskTableData>
-                                </TaskTableRow>
-                            );
-                        })
-                        }
-                    </tbody>
-                </TaskTable>
+                                                        <TaskTableData data-content={value.task}>{value.task}</TaskTableData>
+                                                        <TaskTableData className="due" data-content={FormatDue(value.due)}><span>{FormatDue(value.due)}</span></TaskTableData>
+                                                        <TaskTableData data-content={value.priority}><span>{value.priority}</span></TaskTableData>
+                                                        <TaskTableData data-content={value.status}><span>{value.status}</span></TaskTableData>
+                                                        <TaskTableData data-content={value.periodicity}>{value.periodicity}</TaskTableData>
+                                                    </TaskTableRow>
+                                                )}
+                                            </Draggable>
+                                        );
+                                    })
+                                    }
+                                    {provided.placeholder}
+                                </tbody>
+                            )
+                            }
+                        </Droppable>
+                    </TaskTable>
+                </DragDropContext>
             </IconContext.Provider>
         </>
     );
