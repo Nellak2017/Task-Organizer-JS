dc876f0247c6c37ab225219b6b71c6ba64e823cd
Connor Keenum <Nellakprime@gmail.com>
2021-07-14 21:06:25 +0300

    Tried unsucessfully to map the Table and Grid data components. I will have to rethink my approach later, because now I am too tired to think of a good solution.

--- client/task-organizer-js/src/pages/Organizermain/OrganizerMain.js
+++ client/task-organizer-js/src/pages/Organizermain/OrganizerMain.js
@@ -1,14 +1,18 @@
-import { SideNav, InfoSummary, TableContent } from "../../components";
-import { InfoSummaryData, TableContentData } from "../../pages/OrganizerMain/Data";
+import { SideNav, InfoSummary, TableContent, GridContent } from "../../components";
+import { InfoSummaryData, TableContentData, GridContentData } from "../../pages/OrganizerMain/Data";
 // TODO: Add Media Queries for Responsive Design
 // TODO: Find out how to pass props to components passed as props
+// TODO: Find out how to pass the component alias in as a prop so you can have 1 map for all components, otherwise you will need to re-write InfoSummary Component many times passing in new props each time...
+
 const OrganizerMain = () => {
     return ( 
         <>
         <SideNav />
         {InfoSummaryData.map( (value,key) => {
             return (
-                <InfoSummary key={key} MyComponent={TableContent} Data={[value]} ComponentData={TableContentData}/>
+                <>
+                <InfoSummary key={key} MyComponent={GridContent} Data={[value]} ComponentData={GridContentData}/>
+                </>
             );
         })
         }
