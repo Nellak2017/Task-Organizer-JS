87d9f6c8781fae21c7d8aba055f8f13aaec1f6a2
Connor Keenum <Nellakprime@gmail.com>
2021-07-16 15:57:20 +0300

    Added Styles for the Grid Content and the Grid to make it look a little better. It needs the touch of a professional designer. I will get with my friends for better design.

--- client/task-organizer-js/src/components/GridContent/GridContent.elements.js
+++ client/task-organizer-js/src/components/GridContent/GridContent.elements.js
@@ -1,17 +1,55 @@
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export const TaskGrid = styled.section`
     display: grid;
     width: 100%;
-    border: solid 1px red;
-    // Other grid properties
+    grid-template-columns: repeat(4, 1fr [col-start]);
+    column-gap: 1rem;
+    row-gap: 1.7rem;
+
+    & a[data-content="Low"] {color: rgba(0,97,0,1); background-color: rgba(198,239,206);}
+    & a[data-content="Medium"] {color: rgba(156,114,88); background-color:rgba(255,235,156);}
+    & a[data-content="High"] {color: rgba(207,45,6); background-color: rgba(255,199,206);}    
 `;
 
 export const TaskGridItem = styled(Link)`
+    align-self: center;
+    justify-self: center;
+
     display: flex;
     flex-direction: column;
+    justify-content: center;
+
+    padding: 1rem;
+    padding-top: .5rem;
+    border-radius: 1rem;
     color: #f5f5f5;
-    border: solid 1px blue;
+    font-size: 1.2rem;
+    text-align: left;
+    border: solid 3px rgba(68,76,86,.5);
+
+    &:hover {
+        font-weight: bold;
+        border: solid 3px #1a83ff;
+        transition: 50ms;
+        cursor: pointer;
+    }
+
+    & strong {
+        color: black;
+    }
 `;
 
+export const ThreadName = styled.p`
+    color: black;
+    font-size: 1.5rem;
+    font-weight: bold;
+    text-align: center;
+    margin-bottom: .5rem;
+    border-bottom: 2px solid black;
+`;
+
+export const ThreadContent = styled.p`
+    padding-bottom: .1rem;
+`;
