5dc9ca866048330c81b2a1df1c96e2169b98a69b
Connor Keenum <Nellakprime@gmail.com>
2021-07-17 12:24:17 +0300

    Added Planning Assistant Page. It will contain many useful features like Schedule Problem solvers, College Class Planning and Cost estimator, Trip Planning, and more to come later.

---
+++ client/task-organizer-js/src/pages/PlanningAssistant/PlanningAssistant.js
@@ -0,0 +1,18 @@
+// This page will contain algorithms to help with planning
+// * Schedule Automation for Assigning Shifts
+// * Itenerary Generator and Trip Planners for estimating cost of travel
+// * Routing for flights and drives 
+// * College Class Scheduler, Cost Estimator, etc.
+// * General Mind Map for making Tasks with Dependencies
+
+import { SideNav } from "../../components";
+
+const PlanningAssistant = () => {
+    return (
+        <>
+            <SideNav />
+        </>
+    );
+}
+
+export default PlanningAssistant;
