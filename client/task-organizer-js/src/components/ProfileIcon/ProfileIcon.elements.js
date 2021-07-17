fd78ebe7a45cbec75a0dd5108792ccf943809fbf
Connor Keenum <Nellakprime@gmail.com>
2021-07-17 14:03:30 +0300

    Added the styling for the onHover Container for the Profile Picture (Applied to all SideNavs) with Profile Icons

--- client/task-organizer-js/src/components/ProfileIcon/ProfileIcon.elements.js
+++ client/task-organizer-js/src/components/ProfileIcon/ProfileIcon.elements.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 
 export const ProfileContainer = styled(Link)`
     display: flex;
+    position: relative; // This is for the Absolute positioned OnHover Container
     flex-direction: column;
     align-items: center;
     font-size: .75rem;
@@ -11,6 +12,14 @@ export const ProfileContainer = styled(Link)`
     width: 6.5rem;
     border: 0px solid rgba(68,76,86,.5);
     color: #f5f5f5;
+
+    & .showMe {
+        display: none; 
+    }
+    &:hover .showMe{
+        display: block !important;
+        z-index: 999 !important;
+    }
 
     &:hover {
         transition-timing-function: linear; 
@@ -34,4 +43,66 @@ export const ProfileName = styled.span`
     height: 1rem;
     max-width: 5.5rem !important;
     overflow: hidden;
+`;
+
+export const ProfilePopUp = styled.div`
+    position: absolute; // must be absolute so that you can have it below the profile picture
+    top: 5.1rem;
+    right: 1rem; 
+    width: 18rem;
+    height: 24rem;
+    border-radius: 1rem;
+    background-color: rgba(34,39,46,1); 
+
+    & img {
+        width: 100%;
+        height: auto;
+        border-radius: 1rem 1rem 0 0;
+    }
+
+    & a {
+        text-decoration: none;
+        font-size: .75rem;
+        font-weight: 400;
+        color: rgba(108,116,126,1);
+    }
+
+    & a:hover {
+        color: rgba(128,136,146,1);
+    }
+`;
+
+export const ProfilePopUpInnerContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+
+    text-align: center;
+    font-weight: bold;
+    border-radius: 1rem;
+    font-size: 1.5rem;
+
+    & > * {
+        padding-bottom: 1rem;
+    }
+`;
+
+export const EmailFlexContainer = styled(Link)`
+    display: flex;
+    flex-direction: row;
+    padding-left: 1rem;
+    align-items: center;
+
+    & a {
+        padding-left: 1rem;
+    }
+`;
+
+export const OptionLink = styled(Link)`
+    font-size: 1rem !important;
+    color: rgba(200,200,200,1) !important;
+
+    &:hover {
+        color: rgba(255,255,255,1) !important;
+    }
 `;
