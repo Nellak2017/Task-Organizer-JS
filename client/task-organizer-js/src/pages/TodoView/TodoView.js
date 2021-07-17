769c777abbc7bc74be1009c9266fa72c6584d5be
Connor Keenum <Nellakprime@gmail.com>
2021-07-17 22:09:39 +0300

    Added the page for Todo View.

--- client/task-organizer-js/src/pages/TodoView/TodoView.js
+++ client/task-organizer-js/src/pages/TodoView/TodoView.js
@@ -1,9 +1,17 @@
-import { SideNav } from "../../components";
+import { SideNav, TodoViewSubNav, InfoSummary } from "../../components";
+import { InfoSummaryData, TableHeaderData } from "../../pages/TodoView/Data";
 
 const TodoView = () => {
     return (
         <>
             <SideNav />
+            <TodoViewSubNav />
+            {InfoSummaryData.map((value,key) => {
+            return (
+                <InfoSummary key={key} MyComponent={value.component} Data={[value]} TableHeaders={[TableHeaderData]}/>
+            );
+        })
+        }
         </>
     );
 }
