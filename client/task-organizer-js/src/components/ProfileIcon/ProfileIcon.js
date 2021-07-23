39b9402108d0680359e8f98ef2d781bef396917f
Connor Keenum <Nellakprime@gmail.com>
2021-07-23 13:23:09 +0300

    Fixed the nested <a> problem.

--- client/task-organizer-js/src/components/ProfileIcon/ProfileIcon.js
+++ client/task-organizer-js/src/components/ProfileIcon/ProfileIcon.js
@@ -1,10 +1,10 @@
 import { useLocation } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 import { IconContext } from 'react-icons/lib';
 import {
     ProfileContainer,
     ProfilePicture,
     ProfileName,
+    ProfilePopUpContainer,
     ProfilePopUp,
     ProfilePopUpInnerContainer,
     EmailFlexContainer,
@@ -14,44 +14,45 @@ import * as AiIcons from 'react-icons/ai';
 import * as SiIcons from 'react-icons/si';
 
 //TODO: Add Data Validation for the username when adding the route
+//TODO: Get rid of nested links if possible
 const ProfileIcon = ({ data }) => {
 
     const location = useLocation();
 
     return (
-        <>
-            {data.map((value, key) => {
-                return (
-                    <IconContext.Provider value={{ color: '#fff', size: '1.5rem' }}>
-                        <ProfileContainer to={"/Users/" + value.username} key={key}>
-                            <ProfilePicture alt={value.username + " Picture"} src={value.picture} />
-                            <ProfileName>{value.username}</ProfileName>
-                            <ProfilePopUp className="showMe">
-                                <ProfilePopUpInnerContainer>
-                                    <img src={require('../../images/beautiful-scenery-over-the-lake.jpg').default} alt={'Default Profile Picture'} />
-                                    <p>{value.username}</p>
-                                    <EmailFlexContainer to={location.pathname}>
-                                        <AiIcons.AiOutlineMail className="icon" />
-                                        <a href={"https://" + value.email}>{value.email}</a>
-                                    </EmailFlexContainer>
-                                    <EmailFlexContainer to={'/ControlPanel'}>
-                                        <SiIcons.SiGooglesearchconsole className="icon" />
-                                        <OptionLink to={'/ControlPanel'}>{"Control Panel"}</OptionLink>
-                                    </EmailFlexContainer>
-                                    <EmailFlexContainer to={'/LogOut'}>
-                                        <SiIcons.SiGooglesearchconsole className="icon" />
-                                        <OptionLink to={'/LogOut'}>{"Log Out"}</OptionLink>
-                                    </EmailFlexContainer>
-                                </ProfilePopUpInnerContainer>
-                            </ProfilePopUp>
-                        </ProfileContainer>
-                    </IconContext.Provider>
-                );
-            }
-            )
-            }
+        <IconContext.Provider value={{ color: '#fff', size: '1.5rem' }}>
+            <ProfilePopUpContainer>
+                {data.map((value, key) => {
+                    return (
 
-        </>
+                        <ProfileContainer to={"/Users/" + value.username} key={value.username + key}>
+                            <ProfilePicture alt={value.username + " Picture"} src={value.picture} key={value.username + " Picture"} />
+                            <ProfileName key={value.username}>{value.username}</ProfileName>
+                        </ProfileContainer>
+                    );
+                }
+                )
+                }
+                <ProfilePopUp key={data[0].username + "pop-up"} className="showMe">
+                    <ProfilePopUpInnerContainer>
+                        <img src={require('../../images/beautiful-scenery-over-the-lake.jpg').default} alt={'Default Profile'} />
+                        <p>{data[0].username}</p>
+                        <EmailFlexContainer to={location.pathname}>
+                            <AiIcons.AiOutlineMail className="icon" />
+                            <a href={"https://" + data[0].email}>{data[0].email}</a>
+                        </EmailFlexContainer>
+                        <EmailFlexContainer to={'/ControlPanel'}>
+                            <SiIcons.SiGooglesearchconsole className="icon" />
+                            <OptionLink to={'/ControlPanel'}>{"Control Panel"}</OptionLink>
+                        </EmailFlexContainer>
+                        <EmailFlexContainer to={'/LogOut'}>
+                            <SiIcons.SiGooglesearchconsole className="icon" />
+                            <OptionLink to={'/LogOut'}>{"Log Out"}</OptionLink>
+                        </EmailFlexContainer>
+                    </ProfilePopUpInnerContainer>
+                </ProfilePopUp>
+            </ProfilePopUpContainer>
+        </IconContext.Provider>
     );
 }
 
