aa42271515ec3e3f18cd2229453d1be2fa762f4a
Connor Keenum <Nellakprime@gmail.com>
2021-07-24 11:38:10 +0300

    Made a placeholder component for my incomplete pages.

---
+++ client/task-organizer-js/src/components/UnderConstruction/UnderConstruction.elements.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const Styles = styled.div`
+    
+    @import url(https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700);
+    
+    body {
+        background: #ffffff;
+        color:#999;
+        font-family:Roboto;
+        overflow-x: hidden;
+    }
+
+    path {
+        color: #1a83ff;
+    }
+
+    h1{
+        font-weight:600;
+        font-size:27pt;
+        color:#f5f5f5;
+    }
+
+    p{
+        font-weight:300;
+        text-align:center;
+        font-size:15pt;
+    }
+
+    .warning-content {
+	    position:absolute;
+        top:25%;
+        width:100%;
+        height:300px;
+        text-align:center;
+        margin:0; 
+    }
+
+`;
