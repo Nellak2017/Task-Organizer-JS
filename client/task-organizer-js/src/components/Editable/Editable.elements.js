7bdd1ab2b07e020fa6ea07bcd07935aec36d0115
Connor Keenum <Nellakprime@gmail.com>
2021-08-04 00:14:19 +0300

    Made the first Table Cell Input type, the Free Text Editable Cell.

--- client/task-organizer-js/src/components/Editable/Editable.elements.js
+++ client/task-organizer-js/src/components/Editable/Editable.elements.js
@@ -1,37 +1,17 @@
 import styled from 'styled-components';
 
-export const Styles = styled.div`
-    padding: 1rem;
-    table {
-        border-spacing: 0;
-        border: 1px solid black;
-        tr {
-            :last-child {
-                td {
-                    border-bottom: 0;
-                }
-            }
-        }
-    th,
-    td {
-        margin: 0;
-        padding: 0.5rem;
-        border-bottom: 1px solid black;
-        border-right: 1px solid black;
-            :last-child {
-                border-right: 0;
-            }
-        input {
-            font-size: 1rem;
-            padding: 0;
-            margin: 0;
-            border: 0;
-        }
-    }
-}
-    .pagination {
-        padding: 0.5rem;
-    }
-`
+export const StyledEditableCell = styled.input`
+    border: none;
+    background: transparent;
+    text-align: center;
+    color: white;
+`;
 
-export default Styles;
+export default StyledEditableCell;
+
+/* Need: 
+Due -> Date picker, // https://material-ui-pickers.dev/getting-started/usage
+Time to Complete -> time picker, // https://material-ui-pickers.dev/getting-started/usage
+Priority, Periodicity, Pipelinable -> Drop Down Selection
+
+*/ 
