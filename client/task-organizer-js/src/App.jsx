62189c05a7d89c4c4d4ae09281a815951f2081f5
Connor Keenum <Nellakprime@gmail.com>
2021-08-04 20:57:41 +0300

    Added Global moment util access in App.jsx.

--- client/task-organizer-js/src/App.jsx
+++ client/task-organizer-js/src/App.jsx
@@ -1,4 +1,6 @@
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment';
 import GlobalStyle from './globalStyles';
 import HomePage from './pages/HomePage/HomePage';
 import OrganizerMain from './pages/OrganizerMain/OrganizerMain';
@@ -16,26 +18,28 @@ import ScrollToTop from './components/ScrollToTop';
 
 const App = () => {
 
-  return ( 
-    <Router>
-          <GlobalStyle/>
-          <ScrollToTop />
-          <Switch>
-            <Route path="/" exact component={HomePage}></Route>
-            <Route path="/Calendars" exact component={Calendars}></Route>
-            <Route path="/Gantts" exact component={Gantts}></Route>
-            <Route path="/OrganizerMain" exact component={OrganizerMain}></Route>
-            <Route path="/Settings" exact component={Settings}></Route>
-            <Route path="/Stats" exact component={Stats}></Route>
-            <Route path="/Templates" exact component={Templates}></Route>
-            <Route path="/ThreadView" exact component={ThreadView}></Route>
-            <Route path="/Timelines" exact component={Timelines}></Route>
-            <Route path="/TodoView" exact component={TodoView}></Route>
-            <Route path="/PlanningAssistant" exact component={PlanningAssistant}></Route>
-            <Route path="/Trackers" exact component={Trackers}></Route>
-          </Switch>
-    </Router>
-   );
+  return (
+    <MuiPickersUtilsProvider utils={MomentUtils} >
+      <Router>
+        <GlobalStyle />
+        <ScrollToTop />
+        <Switch>
+          <Route path="/" exact component={HomePage}></Route>
+          <Route path="/Calendars" exact component={Calendars}></Route>
+          <Route path="/Gantts" exact component={Gantts}></Route>
+          <Route path="/OrganizerMain" exact component={OrganizerMain}></Route>
+          <Route path="/Settings" exact component={Settings}></Route>
+          <Route path="/Stats" exact component={Stats}></Route>
+          <Route path="/Templates" exact component={Templates}></Route>
+          <Route path="/ThreadView" exact component={ThreadView}></Route>
+          <Route path="/Timelines" exact component={Timelines}></Route>
+          <Route path="/TodoView" exact component={TodoView}></Route>
+          <Route path="/PlanningAssistant" exact component={PlanningAssistant}></Route>
+          <Route path="/Trackers" exact component={Trackers}></Route>
+        </Switch>
+      </Router>
+    </MuiPickersUtilsProvider>
+  );
 }
- 
+
 export default App;
