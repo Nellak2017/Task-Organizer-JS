1bef1da72c7c3cc7b279074510c755c653b2c3b5
Connor Keenum <Nellakprime@gmail.com>
2021-08-04 20:59:27 +0300

    Added in Logic to handle the Material UI picker. Still incomplete, working on it.

--- client/task-organizer-js/src/components/TableContent/TableContent.js
+++ client/task-organizer-js/src/components/TableContent/TableContent.js
@@ -15,7 +15,7 @@ import Editable from '../../components/Editable/Editable.js';
 import StyledEditableCell from '../Editable/Editable.elements.js';
 
 const TableContent = ({ data, tableHeaders }) => {
-    
+
     const [mutatedData, setMutatedData] = useState(data); // (2) [{task:.., due:...,...},{...}]
     const columns = useMemo(() => tableHeaders[0], []); // (13) [{Header:"Task",accessor:"task"}}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
 
@@ -43,10 +43,10 @@ const TableContent = ({ data, tableHeaders }) => {
     }) => {
         // We need to keep and update the state of the cell normally
         const [value, setValue] = React.useState(initialValue)
-        const onChange = e => { setValue(e.target.value)}
+        const onChange = e => { setValue(e.target.value) }
 
         // We'll only update the external data when the input is blurred
-        const onBlur = () => { updateMyData(index, id, value)}
+        const onBlur = () => { updateMyData(index, id, value) }
 
         // If the initialValue is changed external, sync it up with our state
         React.useEffect(() => { setValue(initialValue) }, [initialValue])
@@ -54,10 +54,10 @@ const TableContent = ({ data, tableHeaders }) => {
         return <StyledEditableCell value={value} onChange={onChange} onBlur={onBlur} />
     }
     const defaultColumn = { Cell: EditableCell }
-    
+
     const instance = useTable({
         columns,
-        data:mutatedData,
+        data: mutatedData,
         defaultColumn,
         updateMyData,
     })
@@ -68,30 +68,6 @@ const TableContent = ({ data, tableHeaders }) => {
         rows,
         prepareRow
     } = instance
-    /*
-    console.log(instance);
-    */
-    
-    const EditableCell2 = ({
-        value: initialValue,
-        row: { index },
-        column: { id },
-        updateMyData, // This is a custom function that we supplied to our table instance
-    }) => {
-        // We need to keep and update the state of the cell normally
-        const [value, setValue] = React.useState(initialValue)
-        const onChange = e => { setValue(e.target.value)}
-
-        // We'll only update the external data when the input is blurred
-        const onBlur = () => { updateMyData(index, id, value)}
-
-        // If the initialValue is changed external, sync it up with our state
-        React.useEffect(() => { setValue(initialValue) }, [initialValue])
-
-        return <input value={value} onChange={onChange} onBlur={onBlur} />
-    }
-
-    
 
     const completeTask = (e, index) => {
         e.preventDefault();
@@ -101,14 +77,14 @@ const TableContent = ({ data, tableHeaders }) => {
     // TODO: Only update the Data that needs to be updated, without losing Id and other pieces of data. 
     const handleOnDragEnd = (result) => {
         if (!result.destination) return;
-        const items = Array.from(rows.map(item => {return (item.values)}));
+        const items = Array.from(rows.map(item => { return (item.values) }));
         const [reorderedItem] = items.splice(result.source.index, 1);
         items.splice(result.destination.index, 0, reorderedItem);
         setMutatedData(items);
     }
-    
-    // TODO: Fix the problem of too many TableDatas in the Mapping, find a simpler way or just do if...thens for all inline (ugly!)
-    // put back Headers[0] if you can't make it work otherwise
+
+    console.log(instance);
+
     return (
         <IconContext.Provider value={{ color: '#fff', size: '2.5rem' }}>
             <DragDropContext onDragEnd={handleOnDragEnd}>
@@ -128,15 +104,13 @@ const TableContent = ({ data, tableHeaders }) => {
                         ))}
                     </thead>
 
-
                     <Droppable droppableId="Task Summaries">
                         {(provided) => (
                             <tbody {...getTableBodyProps()} {...provided.droppableProps} ref={provided.innerRef}>
                                 {rows.map((row, key) => {
                                     prepareRow(row)
-
                                     return (
-                                        <Draggable key={row.cells[key].value} draggableId={row.cells[key].value + key} index={key}>
+                                        <Draggable key={row.cells[key].value} draggableId={String(row.cells[key].value) + String(key)} index={key}>
                                             {provided => (
                                                 <TaskTableRow {...row.getRowProps()} {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
                                                     <TaskTableData className="iconTd">{mutatedData[key].status === "Completed" ? <BiIcons.BiCheckboxChecked key={key} className="icon" onClick={(e) => completeTask(e, key)} />
@@ -145,17 +119,17 @@ const TableContent = ({ data, tableHeaders }) => {
                                                     {
 
                                                         row.cells.map((tableHeader, index) => {
-
+                                                            // We need to handle the edge cases for data content highlighting
+                                                            // This means that for due, we need to be able to parse the Material-UI value Whatever that means
                                                             return (
                                                                 <TaskTableData {...tableHeader.getCellProps()} key={index}
                                                                     data-content={
                                                                         tableHeader.column.Header.toLowerCase().trim() === "due" ?
-                                                                            FormatDue(MakeDue(...tableHeader.value)).toLowerCase().trim() :
+                                                                            FormatDue(tableHeader.value) :
                                                                             tableHeader.value.toLowerCase().trim()
                                                                     }>
                                                                     <span>
-                                                                        {tableHeader.column.Header.toLowerCase().trim() === "due" ?
-                                                                            FormatDue(MakeDue(...tableHeader.value)).trim() :
+                                                                        {
                                                                             tableHeader.render('Cell')
                                                                         }
                                                                     </span>
