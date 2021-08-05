d8b1c2ba2a20444802f9b52e1fcfe2f65afa572c
Connor Keenum <Nellakprime@gmail.com>
2021-08-05 10:48:36 +0300

    Fixed the FormatDue Method to produce the correct number of days in the future something is (Integer number of days, intutive number of days in future -> 12 am next day is 1 day in future)

--- client/task-organizer-js/src/lib/moment/FormatDue.js
+++ client/task-organizer-js/src/lib/moment/FormatDue.js
@@ -3,54 +3,27 @@ import moment from 'moment';
 export const FormatDue = (time) => {
     // input: moment
     // output: (Today/Yesterday/n days ago) at {time.hour}:{time.minute} (pm/am) as a String
-    
-    // Note: This is the version I am using with the date, time picker from Material UI
-    // This means that it has been heavily altered to reflect the new library
-    // The old output is supposed to be time.format("..."), now it is just "..."
 
-    console.log("Time");
-    console.log(time);
-
-    // if input is a string, convert it to a moment object to be used
-    if (typeof time === 'string' || time instanceof String) {
-        time = moment(time);
-    }
-
-     // if input is a Date, convert it to a moment object to be used
-    if (time instanceof Date) {
-        time = moment(time);
-    }
+    // if input is a string or Date, convert it to a moment object to be used
+    if (typeof time === 'string' || time instanceof String || time instanceof Date) { time = moment(time); }
 
     const today = moment();
-    //const defaultText = ` at ${time.format('h')}:${time.format('mm a')}`;
-    const matchesDay = time.date() === today.date();
-    const matchesMonth = time.month() === today.month();
-    const matchesYear = time.year() === today.year();
     const daysInFuture = time.diff(today, 'days', true);
-    const daysInPast = today.add(1, 'days').diff(time, 'days');
-
-    console.log("Days in the future");
-    console.log(daysInFuture);
-
-    /*
-        time_until_tomorrow = start_of_tomorrow_moment - time // positive number greater than current time unless it is 12 am 
-        time_since_yesterday = end_of_yesterday_moment - time // negative number less than current time unless it is 12 am
-
-        n_days_from_now = ... // will be an integer representing what the difference between today and the input is.. ex: 12:01 am tomorrow is tomorrow, instead of 0 days resulting in a bug
-
-     */
+    const timeUntilTomorrow = moment().startOf('day').add(1, 'day').diff(moment(), 'days', true); // positive number greater than current time unless it is 12 am 
+    const timeSinceYesterday = moment().startOf('day').diff(moment(), 'days', true); // negative number less than current time unless it is 12 am
+    const nDaysFromNow = daysInFuture < 0 ? Math.ceil((daysInFuture - timeSinceYesterday) - 1) : Math.floor((daysInFuture - timeUntilTomorrow) + 1);
 
     // Logic to determine the fomatting
-    if (matchesDay && matchesMonth && matchesYear) {
+    if (nDaysFromNow === 0) {
         return "[Today at] h:mm a";
-    } else if (daysInFuture === 1 && matchesMonth && matchesYear) {
+    } else if (nDaysFromNow === 1) {
         return "[Tomorrow at] h:mm a";
-    } else if (daysInPast === 1 && matchesMonth && matchesYear) {
+    } else if (nDaysFromNow === -1) {
         return "[Yesterday at] h:mm a";
-    } else if (daysInFuture > 1) {
-        return "[days from now at] h:mm a";
+    } else if (nDaysFromNow > 1) {
+        return `[${Math.abs(nDaysFromNow)} days from now at] h:mm a`;
     } else {
-        return "[days ago at] h:mm a";
+        return `[${Math.abs(nDaysFromNow)} days ago at] h:mm a`;
     }
 }
 
