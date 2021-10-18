28a0aea93dfcc1c49f54ebe053fcf137835d6ee9
Connor Keenum <nellakprime@gmail.com>
2021-10-18 20:25:30 +0200

    Fixed the Add button table update problem.. Used the Store State as the key so react knows to update the child component.

--- client/task-organizer-js/src/pages/TodoView/TodoView.js
+++ client/task-organizer-js/src/pages/TodoView/TodoView.js
@@ -5,7 +5,8 @@ import { InfoSummaryData, TableHeaderData } from "../../pages/TodoView/Data";
 import { useSelector } from 'react-redux';
 import { store } from "../../state/store";
 
-// TODO: Fix the Redux Component Update Problem so that TodoView can talk to InfoSummaryData
+// TODO: Finish up this page with the other components
+// TODO: Possibly put the full Table in the Redux store, think about it later
 const TodoView = () => {
 
     // Use the State of the Store
@@ -14,65 +15,22 @@ const TodoView = () => {
     // Mirror the State of the Store with InfoSummaryData updates
     const [InfoSummaryDataCopy, setInfoSummaryDataCopy] = useState(InfoSummaryData);
 
-    /* 
-        Right now I have a problem implementing this redux solution of TodoView talking to InfoSummaryData
-        I made my action creator, my reducer, and my store too.. 
-        BUT..
-        I can't make this component update when the store is updated as well..
-
-        possible solutions??
-
-        Research React to see why it wouldn't update, immediately
-        Force update
-        Make the store exactly like the passed in data to the component
-        Add a new prop to the Component
-        Pass the state by lifting state 
-        Just use the Context API
-
-        Implementing the Force update "solution" I got this error:
-
-        "Warning: Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function.
-        at TodoView (http://localhost:3000/static/js/main.chunk.js:10115:80)"
-
-        Has something to do with UseEffect hook maybe??
-
-        https://stackoverflow.com/questions/53949393/cant-perform-a-react-state-update-on-an-unmounted-component
-    */
-
-    // Test this Force update method
-    const [, updateState] = useState();
-    const forceUpdate = useCallback(() => updateState({}), []);
-
-    // Supposedly this is called when the Store Changes.. but it doesn't seem to update the Component!
+    // This is called when the Store Changes.. 
+    // It updates the child component if you put the relevant store information as a 
+    // JSON string in the key prop! If you don't, then React doesn't know when to update the component
     store.subscribe( () => {
-        let copy = InfoSummaryData;
+        const copy = InfoSummaryData;
         copy[0].data = store.getState().TodoViewTable;
-        setInfoSummaryDataCopy([...copy]);
-
-        console.log("Inside Unsubscribe");
-
-        console.log("The InfoSummaryDataCopy");
-        console.log(InfoSummaryDataCopy);
-
-        forceUpdate();
-        console.log("I Literally Just forced an update inside Subscribe");
+        setInfoSummaryDataCopy(copy.slice());
     });
-
-
-    console.log("BEFORE UPDATING STORE:");
-    console.log(state.TodoViewTable);
-
-    console.log("InfoSummaryDataCopy:")
-    console.log(InfoSummaryDataCopy[0].data);
 
     return (
         <>
-
             <SideNav />
             <TodoViewSubNav />
             {InfoSummaryDataCopy.map((value,key) => {
             return (
-                <InfoSummary key={key} MyComponent={value.component} Data={[value]} TableHeaders={[TableHeaderData]}/>
+                <InfoSummary key={JSON.stringify(state.TodoViewTable)} MyComponent={value.component} Data={[value]} TableHeaders={[TableHeaderData]}/>
             );
         })
         }
