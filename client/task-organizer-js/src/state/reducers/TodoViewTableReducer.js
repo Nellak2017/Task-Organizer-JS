c4b8293741e5cc074e65316101d6b7d955962f83
Connor Keenum <nellakprime@gmail.com>
2021-10-21 22:30:01 +0200

    Updated the Reducer function to account for the new TableData Update Action.

--- client/task-organizer-js/src/state/reducers/TodoViewTableReducer.js
+++ client/task-organizer-js/src/state/reducers/TodoViewTableReducer.js
@@ -1,9 +1,14 @@
 import { TableContentData } from "../../pages/TodoView/Data";
 
+const TodoView_AddRow = 'TodoView/AddRow';
+const TodoView_UpdateTableData = 'TodoView/UpdateTableData';
+
 export const reducer = (state = TableContentData , action) => {
     switch (action.type) {
-        case "TodoView/AddRow":
+        case TodoView_AddRow:
             return [...state, action.item];
+        case TodoView_UpdateTableData:
+            return [...action.item];
         default:
             return state;
     }
