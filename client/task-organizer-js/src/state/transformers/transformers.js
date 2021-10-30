e30c08261172b0497ab6f40172aa3cedbab9a28d
Connor Keenum <nellakprime@gmail.com>
2021-10-31 00:34:06 +0200

    Added so-called transformers file. It contains all the functions that will be used to translate the data in the store into something readable to each component for each view.

---
+++ client/task-organizer-js/src/state/transformers/transformers.js
@@ -0,0 +1,14 @@
+// This file contains all the functions that will return a representation of the store state 
+// that is compatible with a component
+
+export const OrganizerMainTableSummary = () => {
+    return ( 0 );
+}
+
+export const OrganizerMainThreadSummary = () => {
+    return ( 0 );
+}
+
+export const TodoViewTasksTable = () => {
+    return ( 0 );
+}
