f234d8bf88e61fea4bad6cdbb6969e86d7398f63
Connor Keenum <nellakprime@gmail.com>
2021-10-31 01:15:53 +0200

    I made the OrganizerMain Table Transformer, that will convert Master Data into data readable to OrganizerMain, by filtering all data I don't need from Master.

--- client/task-organizer-js/src/state/transformers/transformers.js
+++ client/task-organizer-js/src/state/transformers/transformers.js
@@ -1,14 +1,36 @@
 // This file contains all the functions that will return a representation of the store state 
 // that is compatible with a component
 
-export const OrganizerMainTableSummary = () => {
-    return ( 0 );
+// Helper Method that will take a single Object and return another Object with only the allowed keys
+const filterObject = (rawData, allowedData) => {
+    return (
+        Object.keys(rawData)
+            .filter(key => allowedData.includes(key))
+            .reduce((obj, key) => {
+                obj[key] = rawData[key];
+                return obj;
+            }, {})
+    );
 }
 
-export const OrganizerMainThreadSummary = () => {
-    return ( 0 );
+export const OrganizerMainTableSummary = (MasterData) => {
+    // These are the allowed table columns for OrganizerMain
+    const allowed = ['task', 'due', 'priority', 'status', 'periodicity'];
+    
+    return (
+        // For each Object in the Master Data, apply the filterObject function to filter out the items that are irrelevant to this page
+        MasterData.map((value,key) => {
+            return (
+                filterObject(value, allowed)
+            );
+        })
+    );
 }
 
-export const TodoViewTasksTable = () => {
-    return ( 0 );
+export const OrganizerMainThreadSummary = (MasterData) => {
+    return (0);
+}
+
+export const TodoViewTasksTable = (MasterData) => {
+    return (0);
 }
