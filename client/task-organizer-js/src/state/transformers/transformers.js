bf64e68088bba2b7a25886393dc18c0a3ee9703a
Connor Keenum <nellakprime@gmail.com>
2021-10-31 21:44:13 +0200

    Added a higher-order function that will take a string name as input and return a function that returns a Component

--- client/task-organizer-js/src/state/transformers/transformers.js
+++ client/task-organizer-js/src/state/transformers/transformers.js
@@ -1,6 +1,8 @@
 // This file contains all the functions that will return a representation of the store state 
 // that is compatible with a component
 
+import { TableContent, GridContent } from "../../components";
+
 // Helper Method that will take a single Object and return another Object with only the allowed keys
 const filterObject = (rawData, allowedData) => {
     return (
@@ -13,10 +15,23 @@ const filterObject = (rawData, allowedData) => {
     );
 }
 
+// Transforms a Component name in the store into the proper Component by passing it into this higher-order function here
+export const nameToComponentFunction = (name) => {
+    switch (name) {
+        case "TableContent":
+            return ({ data, tableHeaders }) => { return <TableContent data={data} tableHeaders={tableHeaders} /> }
+        case "GridContent":
+            return ({ data }) => { return <GridContent data={data} /> }
+        default:
+            return () => { return <div>Sorry, but the Component you tried to make is undefined. Check the Component param inside of your Store.</div>}
+    }
+};
+
+
 export const OrganizerMainTableSummary = (MasterData) => {
     // These are the allowed table columns for OrganizerMain
     const allowed = ['task', 'due', 'priority', 'status', 'periodicity'];
-    
+
     return (
         // For each Object in the Master Data, apply the filterObject function to filter out the items that are irrelevant to this page
         MasterData.map((value,key) => {
