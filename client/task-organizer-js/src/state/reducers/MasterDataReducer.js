66fc56914976221cfb478acb8ab916986e70cec5
Connor Keenum <nellakprime@gmail.com>
2021-10-31 22:25:38 +0200

    Made it barebones so it only returns state

--- client/task-organizer-js/src/state/reducers/MasterDataReducer.js
+++ client/task-organizer-js/src/state/reducers/MasterDataReducer.js
@@ -1,11 +1,19 @@
 import { masterData } from "../masterData/masterData.js";
 
 const Update_Master_Store_Data = 'App/UpdateMasterStoreData';
+const TodoView_AddRow = 'TodoView/AddRow';
+const TodoView_UpdateTableData = 'TodoView/UpdateTableData';
 
-export const reducer = (state = masterData , action) => {
+// Todo: fix the reducer to actually correct with the new Stores considered
+
+export const reducer = (state = masterData, action) => {
     switch (action.type) {
         case Update_Master_Store_Data:
-            return [action.item];
+            return state;
+        case TodoView_AddRow:
+            return state;
+        case TodoView_UpdateTableData:
+            return state;
         default:
             return state;
     }
