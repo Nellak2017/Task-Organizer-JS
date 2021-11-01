20b805f11e85d6e92cad60e2e4372709448f2911
Connor Keenum <nellakprime@gmail.com>
2021-11-01 20:33:02 +0200

    Added TodoView_UpdateTableData.

--- client/task-organizer-js/src/state/reducers/MasterDataReducer.js
+++ client/task-organizer-js/src/state/reducers/MasterDataReducer.js
@@ -3,8 +3,6 @@ import { masterData } from "../masterData/masterData.js";
 const TodoView_AddRow = 'TodoView/AddRow';
 const TodoView_UpdateTableData = 'TodoView/UpdateTableData';
 
-// Todo: fix the reducer to actually correct with the new Stores considered
-
 export const reducer = (state = masterData, action) => {
     switch (action.type) {
         case TodoView_AddRow:
