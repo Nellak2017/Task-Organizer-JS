5b47a895ac2a7ab2e84938fa6575549293fd90e7
Connor Keenum <nellakprime@gmail.com>
2021-11-04 17:17:41 +0100

    Sucessfully tested the Config Adapter/Transformer function in TodoView. We are using the state of the Store to derive the layout of the app. (Except we are hard coding it and using JSON for GidContent Data. I and such. Next update will address this.

--- client/task-organizer-js/src/pages/Organizermain/OrganizerMain.js
+++ client/task-organizer-js/src/pages/Organizermain/OrganizerMain.js
@@ -1,57 +1,36 @@
 import { SideNav, InfoSummary } from "../../components";
-import { InfoSummaryData, TableHeaderData } from "../../pages/OrganizerMain/Data";
+import { InfoSummaryData, TableHeaderData, GridContentData } from "../../pages/OrganizerMain/Data";
 import { OrganizerMainTableSummary, plainConfigsToUsableConfigs } from "../../state/transformers/transformers.js";
 
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import { useSelector } from 'react-redux'; 
 import { store } from "../../state/store";
 
 
 import * as RiIcons from 'react-icons/ri'; // ONLY FOR TESTING THE NEW FUNCTIONS!
 
+
 // TODO: Add Media Queries for Responsive Design
 // NOTE: MAKE SURE TO PASS COMPONENTS OF THE FORM ({props}) => {return <Component props={props}>} FROM Data TO MyComponent SO NO ERRORS OCCUR! IT TOOK 3 HOURS OF DEBUGGING TO FIGURE OUT NOT TO PASS OBJECTS BUT COMPONENTS TO THIS PROP!
+// Todo: Add a loop that will add the appropriate data, instead of doing it by hand like right now, as in line 23 and 24
 const OrganizerMain = () => {
 
     // Use the State of the Store
     const state = useSelector((state) => state);
     
+    // Manually inject the initial data into our transformed Store data
+    let StoreCopy = plainConfigsToUsableConfigs(state.MasterConfigs.OrganizerMain);
+    StoreCopy[0].data = OrganizerMainTableSummary(state.MasterData);
+    StoreCopy[1].data = GridContentData;
+
     // Mirror the State of the Store with InfoSummaryData updates
-    const [InfoSummaryDataCopy, setInfoSummaryDataCopy] = useState(InfoSummaryData);
-
-    // TESTING the plainConfigsToUsableConfigs function
-
-    const mockData = [
-        {
-            title: 'High Priority Tasks',
-            icon: '',
-            iconComponent: '',
-            text: 'See All',
-            link: 'TodoView',
-            tableHeaders: ['task','due','priority','status','periodicity'],
-            data: '',
-            component: "TableContent"
-        },
-
-        {
-            title: 'Thread Summary',
-            icon: <RiIcons.RiTodoLine className="icon" />,
-            iconComponent: '',
-            text: '',
-            link: '',
-            tableHeaders: '',
-            data: '',
-            component: "GridContent"
-        }
-
-    ];
-    console.log("f(mockData) -> ??? ");
-    console.log(plainConfigsToUsableConfigs(mockData));
+    const [InfoSummaryDataCopy, setInfoSummaryDataCopy] = useState(StoreCopy);
 
     // Every time the store updates, inject the InfoSummaryDataCopy with the OrganizerMain perspective of the Store
     store.subscribe( () => {
-        let copy = InfoSummaryData;
-        copy[0].data = OrganizerMainTableSummary(store.getState().MasterData); // The data in this table is subset of Master Data
+        let copy = StoreCopy;
+        copy[0].data = OrganizerMainTableSummary(state.MasterData); // The data in this table is subset of Master Data
+        copy[1].data =  GridContentData;
         setInfoSummaryDataCopy(copy.slice());
     });
 
