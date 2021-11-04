61fe6619b0526ffeffea0bb92c71dcdbf6cd08b6
Connor Keenum <nellakprime@gmail.com>
2021-11-04 16:21:35 +0100

    Made a Config Transformer/Adapter that takes plain object Redux Store input and outputs the correct InfoSummaryCopy. I currently am Testing it and have got a version available for TableContent Components.

--- client/task-organizer-js/src/state/transformers/transformers.js
+++ client/task-organizer-js/src/state/transformers/transformers.js
@@ -2,6 +2,12 @@
 // that is compatible with a component
 
 import { TableContent, GridContent } from "../../components";
+import { TableHeaderData, DefaultRowTemplate } from "../../pages/TodoView/Data.js";
+
+
+// -----------------------------------------------------------------------------------
+// -------------------------------- HELPER FUNCTIONS ---------------------------------
+// -----------------------------------------------------------------------------------
 
 // Helper Method that will take a single Object and return another Object with only the allowed keys
 const filterObject = (rawData, allowedData) => {
@@ -19,14 +25,76 @@ const filterObject = (rawData, allowedData) => {
 export const nameToComponentFunction = (name) => {
     switch (name) {
         case "TableContent":
-            return ({ data, tableHeaders }) => { return <TableContent data={data} tableHeaders={tableHeaders} /> }
+            return ({ data, tableHeaders }) => { return <TableContent data={data} tableHeaders={tableHeaders} templates={DefaultRowTemplate} /> }
         case "GridContent":
             return ({ data }) => { return <GridContent data={data} /> }
         default:
-            return () => { return <div>Sorry, but the Component you tried to make is undefined. Check the Component param inside of your Store.</div>}
+            return () => { return <div>Sorry, but the Component you tried to make is undefined. Check the Component param inside of your Store.</div> }
     }
 };
 
+// -----------------------------------------------------------------------------------
+// -------------------------------- CONFIG TRANSFORMERS ------------------------------
+// -----------------------------------------------------------------------------------
+
+
+// Adapter function that will convert plain JSON configs from store 
+// into the format that will be used by the application
+// Note: This assumes that we have a list of InfoSummaries being used
+// Note: we will have to solve the Adapter issue, as it likely doesn't scale
+// Note: most likely solution to the Adapter issue, is to add Component to
+//       all input config data so that you can adapt properly no matter what 
+
+export const plainConfigsToUsableConfigs = (plainConfigs) => {
+
+    let ADAPTED = [];
+    for (let item in plainConfigs) {
+
+        // This function must take into account the different types of components for a passed in Configs file
+        switch (plainConfigs[item].component) {
+            case "TableContent":
+                // Get Proper Headers
+                // input: [allowed headers] , output: [non-plain injected obj] called adaptedTableHeaders
+                const masterConfig = TableHeaderData; // Array of objects, contains master copy of tableHeaders
+                const allowedTableHeaders = plainConfigs[item].tableHeaders; // Array of Strings, list of allowed values
+                let adaptedTableHeaders = []; // table headers for Component
+    
+                for (let row in masterConfig) {
+                    const item = masterConfig[row].Header.toLowerCase();
+                    if (allowedTableHeaders.includes(item)) { adaptedTableHeaders.push(masterConfig[row]); }
+                }
+                // Inject Proper Headers and Component
+                // Note, Data will be injected later by something else
+                const adaptedObject = {
+                    title: plainConfigs[item].title,
+                    icon: plainConfigs[item].icon,
+                    iconComponent: plainConfigs[item].iconComponent,
+                    text: plainConfigs[item].text,
+                    link: plainConfigs[item].link,
+                    tableHeaders: adaptedTableHeaders,
+                    data: '',
+                    component: nameToComponentFunction("TableContent")
+                }
+                ADAPTED.push(adaptedObject);
+                break;
+    
+            case "GridContent":
+                console.log("Passing in Transformers.js, plain configs to useable configs, GridContent switch case ");
+                ADAPTED.push(plainConfigs[item]);
+                break;
+            default:
+                ADAPTED.push(plainConfigs[item]);
+                break;
+        }
+    }
+    return ADAPTED;
+}
+
+
+
+// -----------------------------------------------------------------------------------
+// -------------------------------- DATA TRANSFORMERS --------------------------------
+// -----------------------------------------------------------------------------------
 
 export const OrganizerMainTableSummary = (MasterData) => {
     // These are the allowed table columns for OrganizerMain
@@ -34,7 +102,7 @@ export const OrganizerMainTableSummary = (MasterData) => {
 
     return (
         // For each Object in the Master Data, apply the filterObject function to filter out the items that are irrelevant to this page
-        MasterData.map((value,key) => {
+        MasterData.map((value, key) => {
             return (
                 filterObject(value, allowed)
             );
