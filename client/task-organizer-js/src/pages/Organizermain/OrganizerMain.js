d568d9d9d5628ff9bc6aceb7912b75c0fcd16df9
Connor Keenum <nellakprime@gmail.com>
2021-11-04 22:40:40 +0100

    Manually injected all configs and component data, with the exception of Table content. I am doing this by hand and so this is non-optimal.

--- client/task-organizer-js/src/pages/Organizermain/OrganizerMain.js
+++ client/task-organizer-js/src/pages/Organizermain/OrganizerMain.js
@@ -1,36 +1,33 @@
 import { SideNav, InfoSummary } from "../../components";
 import { InfoSummaryData, TableHeaderData, GridContentData } from "../../pages/OrganizerMain/Data";
-import { OrganizerMainTableSummary, plainConfigsToUsableConfigs } from "../../state/transformers/transformers.js";
+import { OrganizerMainTableSummary, plainConfigsToUsableConfigs, OrganizerMainGridSummary } from "../../state/transformers/transformers.js";
 
-import { useState, useEffect} from "react";
-import { useSelector } from 'react-redux'; 
+import { useState } from "react";
+import { useSelector } from 'react-redux';
 import { store } from "../../state/store";
-
-
-import * as RiIcons from 'react-icons/ri'; // ONLY FOR TESTING THE NEW FUNCTIONS!
-
 
 // TODO: Add Media Queries for Responsive Design
 // NOTE: MAKE SURE TO PASS COMPONENTS OF THE FORM ({props}) => {return <Component props={props}>} FROM Data TO MyComponent SO NO ERRORS OCCUR! IT TOOK 3 HOURS OF DEBUGGING TO FIGURE OUT NOT TO PASS OBJECTS BUT COMPONENTS TO THIS PROP!
 // Todo: Add a loop that will add the appropriate data, instead of doing it by hand like right now, as in line 23 and 24
+// Todo: Inject Table Headers so that you can fully depend on the store from now on
 const OrganizerMain = () => {
 
     // Use the State of the Store
     const state = useSelector((state) => state);
-    
+
     // Manually inject the initial data into our transformed Store data
     let StoreCopy = plainConfigsToUsableConfigs(state.MasterConfigs.OrganizerMain);
     StoreCopy[0].data = OrganizerMainTableSummary(state.MasterData);
-    StoreCopy[1].data = GridContentData;
+    StoreCopy[1].data = OrganizerMainGridSummary(state.MasterData);
 
     // Mirror the State of the Store with InfoSummaryData updates
     const [InfoSummaryDataCopy, setInfoSummaryDataCopy] = useState(StoreCopy);
 
     // Every time the store updates, inject the InfoSummaryDataCopy with the OrganizerMain perspective of the Store
-    store.subscribe( () => {
+    store.subscribe(() => {
         let copy = StoreCopy;
         copy[0].data = OrganizerMainTableSummary(state.MasterData); // The data in this table is subset of Master Data
-        copy[1].data =  GridContentData;
+        copy[1].data = OrganizerMainGridSummary(state.MasterData);
         setInfoSummaryDataCopy(copy.slice());
     });
 
@@ -39,7 +36,7 @@ const OrganizerMain = () => {
             <SideNav />
             {InfoSummaryDataCopy.map((value, key) => {
                 return (
-                    <InfoSummary key={JSON.stringify(state.MasterData)+key} MyComponent={value.component} Data={[value]} TableHeaders={[TableHeaderData]} />
+                    <InfoSummary key={JSON.stringify(state.MasterData) + key} MyComponent={value.component} Data={[value]} TableHeaders={[TableHeaderData]} />
                 );
             })
             }
