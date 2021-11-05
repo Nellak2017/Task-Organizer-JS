2a3b98574b8dfe91f238d65d1bcc08daa4443008
Connor Keenum <nellakprime@gmail.com>
2021-11-05 23:28:42 +0100

    Added Basic Calendar (untested).

--- client/task-organizer-js/src/pages/Calendars/Calendars.js
+++ client/task-organizer-js/src/pages/Calendars/Calendars.js
@@ -1,14 +1,49 @@
 import { SideNav } from "../../components";
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import { moment } from 'moment';
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+
+/*
+const locales = {
+    "en-US": require("moment/locale/en-US")
+};
+*/
 
 const localizer = momentLocalizer(moment);
+
+const events = [
+    {
+        title: "Big Meeting",
+        allDay: true,
+        start: moment().toDate(),
+        end: moment()
+            .add(1, "days")
+            .toDate(),
+    },
+    {
+        title: "Vacation",
+        allDay: true,
+        start: moment().toDate(),
+        end: moment()
+            .add(7, "days")
+            .toDate(),
+    }
+];
+
 
 const Calendars = () => {
     return (
         <>
             <SideNav />
-
+            <Calendar
+            localizer={localizer}
+            events={events}
+            startAccessor="start"
+            endAccessor="end"
+            style={{ height: 500, margin: "50px" }}
+            />
         </>
     );
 }
