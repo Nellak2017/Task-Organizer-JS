99c9ab71ae2f33198ee8788f077633bbc72b442c
Connor Keenum <nellakprime@gmail.com>
2021-11-05 23:16:28 +0100

    Added localizer.

--- client/task-organizer-js/src/pages/Calendars/Calendars.js
+++ client/task-organizer-js/src/pages/Calendars/Calendars.js
@@ -2,6 +2,8 @@ import { SideNav } from "../../components";
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import { moment } from 'moment';
 
+const localizer = momentLocalizer(moment);
+
 const Calendars = () => {
     return (
         <>
