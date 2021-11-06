3a9cd44b53417da6d5f4108e23027b5ac418a983
Connor Keenum <nellakprime@gmail.com>
2021-11-06 01:13:17 +0100

    Added Todo list

--- client/task-organizer-js/src/pages/Calendars/Calendars.js
+++ client/task-organizer-js/src/pages/Calendars/Calendars.js
@@ -1,15 +1,11 @@
 import { SideNav } from "../../components";
 import { Calendar, momentLocalizer } from 'react-big-calendar';
-import moment  from 'moment';
+import moment from 'moment';
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useState } from "react";
 import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-/*
-const locales = {
-    "en-US": require("moment/locale/en-US")
-};
-*/
 
 const localizer = momentLocalizer(moment);
 
@@ -28,23 +24,71 @@ const events = [
     }
 ];
 
+/*
+TODO:
+sources: 
+https://codesandbox.io/s/jp1931172w?file=/src/index.js,
+https://www.youtube.com/watch?v=KUKyTRYGrnU&t=129s,
+https://duckduckgo.com/?q=dark+theme+calendar&atb=v181-1&iax=images&ia=images&iai=https%3A%2F%2Fcdn.dribbble.com%2Fusers%2F636775%2Fscreenshots%2F3158395%2Ffor_dribbble.png
+https://www.youtube.com/watch?v=e0cZz7rhn7Y
+
+- [ ] Figure out the styles to modify in Styled Component or Wrapper
+- [ ] Use masterData to compute Events (Figure out allDay exception later)
+- [ ] Create a Calendar form Component, use inpiration from here: https://www.youtube.com/watch?v=KUKyTRYGrnU&t=129s,
+https://duckduckgo.com/?q=dark+theme+calendar&atb=v181-1&iax=images&ia=images&iai=https%3A%2F%2Fcdn.dribbble.com%2Fusers%2F636775%2Fscreenshots%2F3158395%2Ffor_dribbble.png
+- [ ] Properly Style the Calendar to be a good Dark theme
+- [ ] Add dnd to the Calendar
+*/
 
 const Calendars = () => {
 
-    const [newEvent, setNewEvent] = useState({title: "", start: "", end: ""});
-    const [allEvents, setAllEvents] = useState(events)
-    
+    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+    const [allEvents, setAllEvents] = useState(events);
+
+
+    const handleAddEvent = () => {
+        setAllEvents([...allEvents, newEvent]);
+    }
+
 
     return (
         <>
             <SideNav />
             <Calendar
-            localizer={localizer}
-            events={events}
-            startAccessor="start"
-            endAccessor="end"
-            style={{ height: 500, margin: "50px" }}
+                localizer={localizer}
+                events={allEvents}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 500, margin: "50px" }}
             />
+            <h1>Calendar</h1>
+            <h2>Add New Event</h2>
+            <div>
+                <input 
+                type="text" 
+                placeholder="Add Title" 
+                style={{ width: "20%", marginRight: "10px" }}
+                value={newEvent.title} 
+                onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}
+                />
+                <DatePicker
+                placeholderText="Start Date"
+                style={{marginRight: "10px" }}
+                selected={newEvent.start} 
+                onChange={start => setNewEvent({ ...newEvent, start})}
+                />
+                <DatePicker
+                placeholderText="End Date"
+                selected={newEvent.end} 
+                onChange={end => setNewEvent({ ...newEvent, end})}
+                />
+                <button 
+                style={{ marginTop: "10px" }} 
+                onClick={handleAddEvent}
+                >
+                    Add Event
+                </button>
+            </div>
         </>
     );
 }
