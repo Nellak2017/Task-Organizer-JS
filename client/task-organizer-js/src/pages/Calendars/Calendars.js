8f3077265cea5e28529c2a2cb5f63d22d86e3c2f
Connor Keenum <nellakprime@gmail.com>
2021-11-06 21:11:25 +0100

    Extracted out the Calendar Event Form into it's own component.

--- client/task-organizer-js/src/pages/Calendars/Calendars.js
+++ client/task-organizer-js/src/pages/Calendars/Calendars.js
@@ -1,11 +1,12 @@
 import { SideNav } from "../../components";
 import { Calendar } from "../../components/Calendar/Calendar.js";
+import { CalendarEventForm } from "../../components/CalendarEventForm/CalendarEventForm.js"
 import { momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 //import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useState } from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+//import DatePicker from "react-datepicker";
+//import "react-datepicker/dist/react-datepicker.css";
 
 
 const localizer = momentLocalizer(moment);
@@ -47,11 +48,6 @@ const Calendars = () => {
     const [allEvents, setAllEvents] = useState(events);
 
 
-    const handleAddEvent = () => {
-        setAllEvents([...allEvents, newEvent]);
-    }
-
-
     return (
         <>
             <SideNav />
@@ -62,34 +58,7 @@ const Calendars = () => {
                 endAccessor="end"
                 style={{ height: 500, margin: "50px", color: "#fff" }}
             />
-            <h1>Calendar</h1>
-            <h2>Add New Event</h2>
-            <div>
-                <input 
-                type="text" 
-                placeholder="Add Title" 
-                style={{ width: "20%", marginRight: "10px" }}
-                value={newEvent.title} 
-                onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}
-                />
-                <DatePicker
-                placeholderText="Start Date"
-                style={{marginRight: "10px" }}
-                selected={newEvent.start} 
-                onChange={start => setNewEvent({ ...newEvent, start})}
-                />
-                <DatePicker
-                placeholderText="End Date"
-                selected={newEvent.end} 
-                onChange={end => setNewEvent({ ...newEvent, end})}
-                />
-                <button 
-                style={{ marginTop: "10px" }} 
-                onClick={handleAddEvent}
-                >
-                    Add Event
-                </button>
-            </div>
+            <CalendarEventForm/>
         </>
     );
 }
