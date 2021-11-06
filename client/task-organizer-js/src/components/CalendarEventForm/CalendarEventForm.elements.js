b774f5793844748facf73432001c1ec14e3ea76b
Connor Keenum <nellakprime@gmail.com>
2021-11-06 21:56:02 +0100

    I am working on the Event Form Styling. It will be a popup container when some add button is pressed.

--- client/task-organizer-js/src/components/CalendarEventForm/CalendarEventForm.elements.js
+++ client/task-organizer-js/src/components/CalendarEventForm/CalendarEventForm.elements.js
@@ -5,7 +5,24 @@ import "react-datepicker/dist/react-datepicker.css";
 
 
 export const StyledDatePicker = styled(DatePicker)`
-    
+
 `;
  
+export const EventFormContainer = styled.div`
+    position: absolute; // must be absolute so that you can have it in the center easily
+    top: 25%; 
+    right: 40%;
+    width: 25rem;
+    height: 20rem; 
+    border-radius: 1rem;
+    z-index: 999 !important;
+    border: 1px solid white;
+    background-color: rgb(34,39,46);
+    
+    h1,h2,h3,h4,h5,h6 {
+        color: white;
+        text-align: center;
+    }
+`;
+
 export default StyledDatePicker;
