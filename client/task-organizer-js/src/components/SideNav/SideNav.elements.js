cece1e7093325c3ef6225fcc845b94aa8d9e2d41
Connor Keenum <nellakprime@gmail.com>
2021-11-15 17:54:06 -0600

    Made the Side Nav Mobile responsive.

--- client/task-organizer-js/src/components/SideNav/SideNav.elements.js
+++ client/task-organizer-js/src/components/SideNav/SideNav.elements.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import GridOnIcon from '@material-ui/icons/GridOn';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+import { device } from "../../lib/CSS/device.js";
 
 // TODO: Make the sticky positioning work better, fixed sucks
 
@@ -68,19 +70,24 @@ export const TabName = styled.span`
         margin-left: 2rem;
     }
 `;
-
+// This is the Link in the Vertical Navbar
 export const MenuBars = styled(Link)`
-    margin-left: 2rem;
+    margin-left: 1rem;
     font-size: 2rem;
     background: none;
-    color: #f5f5f5 !Important;
+    color: #f5f5f5 ;
+
+    /* Mobiles - Extra Small <425dp - Portrait*/
+    @media ${device.mobileMMax}{
+        text-align: left;
+    }
 `;
 
 // This is the container for the logo and the logo text
 export const NavLogo = styled(Link)`
     color: white;
     background-color: rgba(34,39,46,1);
-    justify-self: flex-start;
+    
     cursor: pointer;
     text-decoration: none;
     font-size: 1rem;
@@ -95,14 +102,25 @@ export const NavLogo = styled(Link)`
         transition: 350ms;
         border-bottom: 2px solid #1a83ff;
     }
+
+    /* Mobiles - Extra Small <425dp - Portrait*/
+    @media ${device.mobileMMax}{
+        justify-content: center;
+        text-align: left;  
+    }
 `;
 
 // This is the Logo for my App 
 export const NavIcon = styled(GridOnIcon)`
-    font-size: 3rem !Important; 
+    font-size: 3rem !important; 
     color: #f5f5f5;
     margin-left: 1rem;
     margin-bottom: 0rem;
+    /* Mobiles - Extra Small <425dp - Portrait*/
+    @media ${device.mobileMMax}{
+        margin-left: 0rem;
+        padding-left: 0rem;
+    }
 `;
 // This is the Logo text for my App
 export const LogoWords = styled.span`
@@ -111,13 +129,21 @@ export const LogoWords = styled.span`
     background: none;
     color: #f5f5f5;
     padding-top: .5rem;
+
+     /* Mobiles - Extra Small <425dp - Portrait*/
+     @media ${device.mobileMMax}{
+        justify-content: start;
+        text-align: left;
+        margin-left: 1rem;
+        padding-left: 1rem;
+    }
 `;
 
 // This is the vertical nav bar
 export const NavMenu = styled.nav`
     z-index: 999; 
     background-color: rgba(16,21,34,1);
-    width: 250px;
+    width: 240px;
     height: 100vh;
     display: flex;
     justify-content: center;
@@ -133,12 +159,19 @@ export const NavMenu = styled.nav`
         left: 0;
         transition: 350ms;
     }
+
+    /* Mobiles - Extra Small <425dp - Portrait*/
+    @media ${device.mobileMMax}{
+        width: 100%;
+        overflow: scroll;
+    }
 `;
 
 export const NavMenuItems = styled.ul`
     width: 100%;
 `;
 
+// Box in the vertical nav that has logo and link name
 export const NavText = styled.li`
     display: flex;
     justify-content: start;
@@ -166,5 +199,16 @@ export const NavText = styled.li`
     & a:hover {
         background-color: #1a83ff;
     }
+
+    /* Mobiles - Extra Small <425dp - Portrait*/
+    @media ${device.mobileMMax}{
+        justify-content: center;
+        text-align: center;
+        padding: .5rem 0px .5rem 0;
+        & a {
+         width:163px;
+         font-size: 1.5rem;  
+        
+        }
+    }
 `;
- 
