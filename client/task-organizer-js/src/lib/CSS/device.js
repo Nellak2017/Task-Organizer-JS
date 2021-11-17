600558cb02c280040fa728c263e1a162ac4cf9b7
Connor Keenum <nellakprime@gmail.com>
2021-11-17 01:35:41 -0600

    Added some edge cases to help render Media Queries with higher gradation.

--- client/task-organizer-js/src/lib/CSS/device.js
+++ client/task-organizer-js/src/lib/CSS/device.js
@@ -15,7 +15,9 @@ export const device = {
     mobileS: `(min-width: ${size.mobileS})`,
     mobileM: `(min-width: ${size.mobileM})`,
     mobileL: `(min-width: ${size.mobileL})`,
+    tabletS: `(min-width: 551px)`,
     tablet: `(min-width: ${size.tablet})`,
+    tabletM: `(min-width: 850px)`,
     laptop: `(min-width: ${size.laptop})`,
     laptopL: `(min-width: ${size.laptopL}`,
     desktop: `(min-width: ${size.desktop})`,
@@ -23,7 +25,9 @@ export const device = {
     mobileSMax: `(max-width: ${size.mobileM})`,
     mobileMMax: `(max-width: ${size.mobileL})`,
     mobileLMax: `(max-width: ${size.tablet})`,
+    tabletSMax: `(max-width: 550px)`,
     tabletMax: `(max-width: ${size.laptop})`,
+    tabletMMax: `(max-width: 850px)`,
     laptopMax: `(max-width: ${size.laptopL})`,
     laptopLMax: `(max-width: ${size.desktop}))`    
 };
