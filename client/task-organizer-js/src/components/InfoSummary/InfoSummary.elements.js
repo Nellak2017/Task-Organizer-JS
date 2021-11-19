e82e0fa790d8bf7c0efe77357161c040548e67cf
Connor Keenum <nellakprime@gmail.com>
2021-11-18 20:44:34 -0600

    Reduced the Contraction time of the table collapse function to 0ms so that when updating data, it doesn't jankily update. On the downside, it instantaneously disappears on every collapse action. (Looks pretty funny)

--- client/task-organizer-js/src/components/InfoSummary/InfoSummary.elements.js
+++ client/task-organizer-js/src/components/InfoSummary/InfoSummary.elements.js
@@ -21,7 +21,6 @@ export const SummaryContainer = styled.div`
 
 // The Place where there is a brief title, dropdown, 
 // and a small button for extra options
-// TODO: Add Media Queries for Responsive Design
 export const SummaryDropDown = styled.section`
     display: flex;
     flex-direction: row;
@@ -84,13 +83,13 @@ export const SummaryLink = styled(Link)`
 // This will let us smoothly transition from nothing to something
 
 const ExpandingAnimation = keyframes`
-    0% { opacity: 0; min-height: 0; padding: 0; margin: 0 0 0 20rem;}
+    0% { opacity: 0; min-height: 0; padding: 0; margin: 0 0 0 0;}
     100% { opacity: 1; min-height: 10rem;}
 `;
 
 const CollapsingAnimation = keyframes`
     0% { opacity: 1; min-height: 10rem;}
-    100% { opacity: 0; min-height: 0; padding: 0; margin: 0 0 0 20rem;
+    100% { opacity: 0; min-height: 0; padding: 0; margin: 0 0 0 0;
 }
 `;
 
@@ -113,14 +112,14 @@ export const SummaryInfoCollapsable = styled.section`
 
     & *{
         ${props => props.expanded ?
-        "transition-timing-function: linear; transition: 200ms;":NestedComponentAnimationFix
+        "transition-timing-function: linear; transition: 400ms;":NestedComponentAnimationFix
         };
         
     }
     display: flex;
     flex-direction: column;
     align-items: stretch;
-    animation-duration: 500ms;
+    animation-duration: 0ms;
     animation-fill-mode: both;
     font-size: 1rem;
     margin-left: 0; //  was 0
