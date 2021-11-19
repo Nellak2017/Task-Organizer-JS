b02039546df87bafe8f60dd365340e8b3bcce9b7
Connor Keenum <nellakprime@gmail.com>
2021-11-19 16:05:40 -0600

    Cleaned up MasterConfigReducer.js.

--- client/task-organizer-js/src/state/reducers/MasterConfigReducer.js
+++ client/task-organizer-js/src/state/reducers/MasterConfigReducer.js
@@ -35,19 +35,3 @@ export const reducer = (state = masterConfigs , action) => {
 };
 
 export default reducer;
-
-/*
-// MasterConfig reducer
-
-import { masterConfig } from "../../state/masterConfig/masterConfig.js"
-
-// Variables that have the names of each Action type
-
-export const reducer = (state = masterConfig, action) => {
-	switch (action.type) {
-		case // First action:
-			return // Action's update to the configs
-		... // For any Config update, submit action here
-	}
-}
-*/
