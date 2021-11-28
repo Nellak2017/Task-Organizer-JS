ae8f6362ffd5a9c7401736a53ce1682258eed626
Connor Keenum <nellakprime@gmail.com>
2021-11-27 23:25:38 -0600

    Started work on making styles for the TodoViewSubNav Custom Side Nav Styles

--- client/task-organizer-js/src/components/TodoViewSubNav/TodoViewSubNav.js
+++ client/task-organizer-js/src/components/TodoViewSubNav/TodoViewSubNav.js
@@ -13,17 +13,30 @@ import {
     CustomMenu,
     ToggleSwitchWrapper,
     ToggleSwitchLabel,
-    ToggleSwitchBox
+    ToggleSwitchBox,
+    CustomLabelContainer,
+    CustomLabel,
+    ArrowBox,
+    ContentContainer,
+    SettingContainer,
+    Label,
+    AddTemplate,
+    TemplateContentBox,
+    TemplateSubtitle,
+    TemplateTextArea,
+    GeneralText
 } from './TodoViewSubNav.elements.js';
-import { useState} from 'react';
+import { useState } from 'react';
 import * as CgIcons from 'react-icons/cg';
 import * as RiIcons from 'react-icons/ri';
 import { IconContext } from 'react-icons/lib';
 import * as GoIcons from 'react-icons/go';
+import * as AiIcons from 'react-icons/ai';
+// AiOutlineArrowRight
 
 import { todoViewAddTask, todoViewDeleteMode } from "../../state/actions/TodoViewActions";
 
-import { useDispatch } from 'react-redux'; 
+import { useDispatch } from 'react-redux';
 
 const TodoViewSubNav = () => {
 
@@ -33,7 +46,7 @@ const TodoViewSubNav = () => {
     const [delPressed, setDelPressed] = useState(false);
     const [customPressed, setCustomPressed] = useState(false);
 
-    const handleAddButtonClick = () => {setAddPressed(!addPressed);}
+    const handleAddButtonClick = () => { setAddPressed(!addPressed); }
 
     const handleAddTaskClick = () => {
         dispatch(todoViewAddTask());
@@ -55,17 +68,17 @@ const TodoViewSubNav = () => {
                     <SubNavAddTaskButton onClick={handleAddTaskClick}>Add Task</SubNavAddTaskButton>
                     <SubNavAddOptionsButton onClick={handleAddButtonClick}>v</SubNavAddOptionsButton>
                     <AddPopUpContainer>
-                        <SubNavAddPopUp className={!addPressed ? "showMe" : "" }>
+                        <SubNavAddPopUp className={!addPressed ? "showMe" : ""}>
                             <AddPopUpInnerContainer>
                                 <AddPopUpButton><CgIcons.CgTemplate />Add Thread</AddPopUpButton>
                                 <AddPopUpButton><RiIcons.RiStackLine />Add Template</AddPopUpButton>
                             </AddPopUpInnerContainer>
-                    
+
                         </SubNavAddPopUp>
                     </AddPopUpContainer>
 
-                    <SubNavDeleteTaskButton onClick={handleDeleteTaskClick}>Turn {!delPressed ? "On":"Off"} Delete Tasks</SubNavDeleteTaskButton>
-                    
+                    <SubNavDeleteTaskButton onClick={handleDeleteTaskClick}>Turn {!delPressed ? "On" : "Off"} Delete Tasks</SubNavDeleteTaskButton>
+
                 </SubNavAddContainer>
 
 
@@ -88,10 +101,42 @@ const TodoViewSubNav = () => {
                 </SubNavOptionsContainer>
 
                 <CustomMenu className={customPressed ? "active" : ""}>
-                    <ToggleSwitchWrapper>
-                        <ToggleSwitchBox id="checkbox" type="checkbox"/>
-                        <ToggleSwitchLabel htmlFor="checkbox"/>
-                    </ToggleSwitchWrapper>
+                    <CustomLabelContainer>
+                        <CustomLabel>
+                            Custom
+                        </CustomLabel>
+                        <ArrowBox>
+                            <AiIcons.AiOutlineArrowRight className="icon"/>
+                        </ArrowBox>
+                    </CustomLabelContainer>
+                    <ContentContainer>
+                        <SettingContainer>
+                            <Label>Templates</Label>
+                            <AddTemplate>+ Add Template</AddTemplate>
+                        </SettingContainer>
+                        <TemplateContentBox>
+                            <TemplateSubtitle>
+                                No Templates Yet
+                            </TemplateSubtitle>
+                            <TemplateTextArea>
+                                Make templates to make tasks quicker so you can focus on the important stuff.
+                            </TemplateTextArea>
+                        </TemplateContentBox>
+                    </ContentContainer>
+                    <ContentContainer>
+                        <SettingContainer>
+                            <Label>Theme</Label>
+                        </SettingContainer>
+                        <ContentContainer>
+                            <SettingContainer>
+                                (I am a setting box)
+                                <ToggleSwitchWrapper>
+                                    <ToggleSwitchBox id="checkbox" type="checkbox" />
+                                    <ToggleSwitchLabel htmlFor="checkbox" />
+                                </ToggleSwitchWrapper>
+                            </SettingContainer>
+                        </ContentContainer>
+                    </ContentContainer>
                 </CustomMenu>
             </SubNav>
         </>
