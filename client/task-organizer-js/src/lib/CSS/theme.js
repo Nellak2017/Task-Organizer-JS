406eeebf9c5820470ccbedf4f341c312c76b5d72
Connor Keenum <nellakprime@gmail.com>
2021-11-28 20:41:45 -0600

    Contains Light and Dark theme Objects that are used to determine the colors of the items in the Application.

---
+++ client/task-organizer-js/src/lib/CSS/theme.js
@@ -0,0 +1,18 @@
+// This is to hold the Themes I will use in the App
+
+// This is the Light theme
+export const LightTheme = {
+    body: '#fff',
+    nav: '#fff',
+    fontColor: '#000'
+};
+
+// This is the Dark theme
+export const DarkTheme = {
+    body: 'rgba(16,21,34,1)',
+    nav: 'rgba(34,39,46,1)',
+    fontColor: '#fff'
+};
+
+
+export default LightTheme;
