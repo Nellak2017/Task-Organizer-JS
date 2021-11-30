0c88e37120d614077d9dbb689c69f70157a8b61b
Connor Keenum <nellakprime@gmail.com>
2021-11-29 22:22:11 -0600

    Added a flex footer that holds some icons without functionality yet.

--- client/task-organizer-js/src/components/ThreadViewLarge/ThreadViewLarge.js
+++ client/task-organizer-js/src/components/ThreadViewLarge/ThreadViewLarge.js
@@ -1,6 +1,7 @@
 import React, { useState, useMemo, useEffect, useRef } from 'react';
 import { IconContext } from 'react-icons/lib';
 import * as BiIcons from 'react-icons/bi'; // BiCheckbox
+import * as BsIcons from 'react-icons/bs'; // BsFillArrowLeftCircleFill and BsFillArrowRightCircleFill
 import {
     MainContainer,
     ThreadBox,
@@ -13,9 +14,25 @@ import {
     ThreadTitle,
     IconContainer,
     TaskNameContainer,
+    PaginationButtonContainer,
     PaginationButton
 } from './ThreadViewLarge.elements.js';
 
+
+/* 
+TODO:
+
+Use the react-table to make a 2 column, editable table, using data from masterData
+    Use Data to filter out a particular thread's information and put title there
+    Use Data to filter out a particular thread's information and put tasks there
+Change See all to be a link to the TodoView Page
+Add onClick for the icons for pagination
+Make Light Theme Friendly
+Make Mobile Responsive
+Make Printer Friendly
+Add Add Feature (That updates master) (TO update master this way, you must modify the current gatekeeping mechanism for making alterations in Table Content.. it currently checks if it matches all of TodoView instead of a editable prop..)
+Add Delete Feature (That updates master)
+*/
 
 export const ThreadViewLarge = ({ tasks }) => {
     const [taskComplete, setTaskComplete] = useState(false);
@@ -57,6 +74,10 @@ export const ThreadViewLarge = ({ tasks }) => {
                                 <TaskNameContainer>I am the next task from the data</TaskNameContainer>
                             </ThreadContainer>
                         </Tasks>
+                        <PaginationButtonContainer>
+                            <BsIcons.BsFillArrowLeftCircleFill className="icon" />
+                            <BsIcons.BsFillArrowRightCircleFill className="icon" />
+                        </PaginationButtonContainer>
                     </ThreadBox>
                 </MainContainer>
             </IconContext.Provider>
